5bf5ac15121cc2f01cee2de2a0568240c916b82e
Jonathan Wright <jpwright@users.sf.net>
2005-10-19 16:02:26 +0000

    adding factor analysis

--- ImageD11/guifactors.py
+++ ImageD11/guifactors.py
@@ -21,14 +21,12 @@
 from Numeric import *
 from Tkinter import *
 
-import time,math,unitcell,sys
+import time,math,sys
 
-import indexing
-
-from listdialog import listdialog
+import factors
 
 
-class guiindexer:
+class guifactors:
                            
    def __init__(self,parent):
       """
@@ -36,154 +34,35 @@ class guiindexer:
       """
       self.parent=parent
 # peaks are in      self.parent.finalpeaks
-      self.menuitems = ( "Indexing", 0,
-                         [ ( "Load g-vectors", 0, self.loadgv),
-                           ( "Plot x/y/z", 5, self.plotxyz     ),
-                           ( "Load parameters", 1, self.loadfileparameters),
-                           ( "Edit parameters", 0, self.editparameters),
-                           ( "Assign peaks to powder rings", 0, self.assignpeaks),
-                           ( "Make Friedel pair file", 5, self.makefriedel),
-                           ( "Generate trial orientations",0, self.find),
-                           ( "Score trial orientations",0, self.scorethem),
-                           ( "Save parameters", 0, self.saveparameters),
-                           ( "Save UBI matrices", 5, self.saveubis),
-                           ( "Write out indexed peaks",0,self.saveindexing )
+      self.menuitems = ( "Factors", 0,
+                         [ ( "Load chi files", 0, self.loadchis),
+                           ( "Plot a chi", 0, self.plotchi     ),
+			   ( "Generate SVD", 0, self.svd ),
+			   ( "Select number of factors", 0, self.setnfactors),
+			   ( "Generate differences", 9, self.dataminuscalc)
                          ] ) 
-      self.parameters={}
-      self.indexer=indexing.indexer(unitcell=self.parent.unitcell,gv=self.parent.gv)
-      self.loadparameters()
-      self.plot3d=None
+      self.factors=factors.factors()
+	
+   def loadchis(self):
+      filename=self.parent.opener.show(title="Name of a chi file")
+      # globs by default
+      self.factors.loadchis(filename)
 
-   def loadgv(self):
-#      if self.parent.gv!=None:
-#         from tkMessageBox import askyesno
-#         if not askyesno("Overwrite current g-vectors?","Loading new will 
-#overwrite the ones in memory now, are you sure?"):
-#            return
-      filename=self.parent.opener.show(title="File containing g-vectors")
-      self.indexer.readgvfile(filename)
+   def plotchi(self):
+      # which one?
+      self.parent.twodplotter.adddata(
+      	( "Chi file", twodplot.data(self.factors.x,self.factors.y[i],
+	{ "xlabel":"probably 2theta",
+	  "ylabel":"probably intensity",
+	  "title":"chifile %d"%(i) } ) )
 
-   def saveubis(self):
-      filename=self.parent.saver.show(title="File to save UBIS")
-      self.indexer.saveubis(filename)
+   def svd(self):
+      self.factors.svd()
+      # Give scree plot
 
-   def makefriedel(self):
-      filename=self.parent.saver.show(title="File to save Friedels")
-      self.indexer.friedelpairs(filename)
+   def setnfactors(self):
+      self.factors.setnfactors(i)
 
-
-
-   def scorethem(self):
-      self.indexer.scorethem()
+   def dataminuscalc(self):
+      self.factors.generate()
       
-   def assignpeaks(self):
-      self.indexer.assigntorings()
-
-   def loadfileparameters(self):
-      filename=self.parent.opener.show(title="File containing indexing parameters")
-      self.loadparameters(filename)
-
-   def loadparameters(self,filename=None):   
-      if filename==None:
-         self.parameters['cosine_tol']=self.indexer.cosine_tol
-         self.parameters['hkl_tol']=self.indexer.hkl_tol
-         self.parameters['ring_1']=self.indexer.ring_1
-         self.parameters['ring_2']=self.indexer.ring_2
-         self.parameters['minpks']=self.indexer.minpks
-         self.parameters['uniqueness']=self.indexer.uniqueness
-         self.parameters['ds_tol']=self.indexer.ds_tol
-         self.parameters['wavelength']=self.indexer.wavelength
-      else:
-         try:
-            lines = open(filename,"r").readlines()
-            for line in lines:
-               name,value=line.split()
-               self.parameters[name]=value
-            self.indexer.cosine_tol=float(self.parameters['cosine_tol'])
-            self.indexer.hkl_tol= float(self.parameters['hkl_tol'])
-            self.indexer.ring_1=int(self.parameters['ring_1'])
-            self.indexer.ring_2=int(self.parameters['ring_2'])
-            self.indexer.minpks=int(self.parameters['minpks'])
-            self.indexer.uniqueness=float(self.parameters['uniqueness'])
-            self.indexer.ds_tol=float(self.parameters['ds_tol'])
-            self.indexer.wavelength=float(self.parameters['wavelength'])
-         except IOError:
-            from tkMessageBox import showinfo
-            showinfo("Sorry","Could not open/interpret your file")
-               
-
-   def saveparameters(self,filename=None):
-      if filename==None:
-         filename=self.parent.saver.show(title="File to save indexing parameters")
-      f=open(filename,"w")
-      keys=self.parameters.keys()
-      keys.sort()
-      for key in keys:
-         try:
-            f.write("%s %f\n"%(key,self.parameters[key]))
-         except:
-            f.write("%s %s\n"%(key,self.parameters[key]))
-      f.close()
-
-   def saveparameters(self,filename=None):
-      if filename==None:
-         filename=self.parent.saver.show(title="File to save indexing parameters")
-      f=open(filename,"w")
-      keys=self.parameters.keys()
-      keys.sort()
-      for key in keys:
-         try:
-            f.write("%s %f\n"%(key,self.parameters[key]))
-         except:
-            f.write("%s %s\n"%(key,self.parameters[key]))
-      f.close()
-
-
-
-
-   def editparameters(self):
-      self.parameters['cosine_tol']=self.indexer.cosine_tol
-      self.parameters['hkl_tol']=self.indexer.hkl_tol
-      self.parameters['ring_1']=self.indexer.ring_1
-      self.parameters['ring_2']=self.indexer.ring_2
-      self.parameters['minpks']=self.indexer.minpks
-      self.parameters['uniqueness']=self.indexer.uniqueness
-      self.parameters['ds_tol']=self.indexer.ds_tol
-      self.parameters['wavelength']=self.indexer.wavelength
-      d=listdialog(self.parent,items=self.parameters,title="Indexing parameters")
-      self.parameters=d.result
-      self.indexer.cosine_tol=float(self.parameters['cosine_tol'])
-      self.indexer.hkl_tol= float(self.parameters['hkl_tol'])
-      self.indexer.ring_1=int(self.parameters['ring_1'])
-      self.indexer.ring_2=int(self.parameters['ring_2'])
-      self.indexer.minpks=int(self.parameters['minpks'])
-      self.indexer.ds_tol=float(self.parameters['ds_tol'])
-      self.indexer.uniqueness=float(self.parameters['uniqueness'])
-      try:
-         self.indexer.wavelength=float(self.parameters['wavelength'])
-      except:
-         self.indexer.wavelength=None
-      
-      
-   def plotxyz(self):
-      """
-      Plots the x,y arrays being used
-      """
-      import plot3d
-      if self.indexer.gv!=None:
-         if self.plot3d==None:
-            self.plot3d = plot3d.plot3d(self.parent,self.indexer.gv)
-            self.plot3d.go()
-            print self.plot3d
-         else:
-            self.plot3d.changedata(self.indexer.gv)
-
-
-   def find(self):
-      self.indexer.find()
-
-
-   def saveindexing(self,filename=None):
-      if filename==None:
-         filename=self.parent.saver.show(title="File to save indexing parameters")
-      self.indexer.saveindexing(filename)
