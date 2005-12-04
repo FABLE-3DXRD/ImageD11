257535608fe3a220ff0224413d4942c3ff2cf273
Jonathan Wright <jpwright@users.sf.net>
2005-12-04 11:29:22 +0000

    *** empty log message ***

--- scripts/edfheader.py
+++ scripts/edfheader.py
@@ -20,7 +20,7 @@ import sys
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+import gzip
 def getheader(filename):
     """
     Reads a header from an edf file in 1024 byte chunks.
@@ -29,7 +29,10 @@ def getheader(filename):
     Adds a filename key at the top
     """
     h="filename = "
-    fp=open(filename,"rb")
+    try:
+        fp=open(filename,"rb")
+    except:
+        fp=gzip.GzipFile(filename+".gz","rb")
     h=h+filename+";\n"
     s=fp.read(1024)
     if s.find("{")==-1:
