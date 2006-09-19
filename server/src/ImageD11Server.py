13410c47bb7d91489ea5ea774241d1025d87ff09
jaro_b <jaro_b@users.sf.net>
2006-09-19 09:46:19 +0000

    Imaged Server working

--- server/src/ImageD11Server.py
+++ server/src/ImageD11Server.py
@@ -1,22 +1,24 @@
+#!/usr/bin/python2.4
+
 import SimpleXMLRPCServer
 
 import guicommand
 
+if __name__=="__main__":
+    #server object
+    guicommander = guicommand.guicommand()
 
-#server object
-guicommander = guicommand.guicommand()
+    #merger_object.readpeaks(testfile)
+    #    object.harvestpeaks()
+    #merger_object.mergepeaks()
+    #merger_object.filter()
+    #merger_object.savepeaks(fltfile)
+    #    sys.exit(0)
+    server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 8888))
+    #server.register_instance(sum_object)
 
-#merger_object.readpeaks(testfile)
-#    object.harvestpeaks()
-#merger_object.mergepeaks()
-#merger_object.filter()
-#merger_object.savepeaks(fltfile)
-#    sys.exit(0)
-server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 8888))
-#server.register_instance(sum_object)
+    server.register_instance(guicommander)
 
-server.register_instance(guicommander)
-
-#Go into the main listener loop
-print "Listening on port 8888"
-server.serve_forever()
+    #Go into the main listener loop
+    print "Listening on port 8888"
+    server.serve_forever()
