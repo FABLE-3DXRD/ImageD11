807c383ac5806fe0725b26d343bade70308512e1
jaro_b <jaro_b@users.sf.net>
2006-09-21 08:36:52 +0000

    asdca

---
+++ server/src/build/lib.linux-i686-2.4/Imaged11Functions/parameters.py
@@ -0,0 +1,129 @@
+
+
+
+
+# ImageD11_v0.4 Software for beamline ID11
+# Copyright (C) 2005  Jon Wright
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+"""
+Class to handle groups of parameters to be saved in and out
+of files and edited in guis with the fixed/varied info etc
+"""
+
+class parameters:
+    def __init__(self,**kwds):
+        """
+        name=value style arg list
+        """
+        self.parameters = kwds
+
+    def set_parameters(self,d):
+        """
+        Updates the values of parameters
+        """
+        self.parameters.update(d)
+        self.dumbtypecheck()
+
+    def get_parameters(self):
+        """
+        Returns a dictionary of parameters
+        """
+        return self.parameters
+
+    def update_yourself(self,other):
+        """
+        Sychronise this parameter objects list of values with another object
+        """
+        for k,v in self.parameters.items():
+            if hasattr(other,k):
+                var = getattr(other,k)
+                print "setting: pars[%s] from %s to %s"%(k,v,var)
+                self.parameters[k]=var
+            else:
+                print "error: %s has no attribute %s, ignoring"%(other,k)
+
+    def update_other(self,other):
+        """
+        Synchronise an object with the values in this object
+        """
+        for k,v in self.parameters.items():
+            if hasattr(other,k):
+                var = getattr(other,k)
+                print "setting: %s.%s from %s to %s"%(other,k,var,v)
+                setattr(other,k,v)
+            else:
+                print "error: %s has no attribute %s, ignoring"%(other,k)
+
+    def saveparameters(self,filename):
+        """
+        Write parameters to a file
+        """
+        f=open(filename,"w")
+        keys=self.parameters.keys()
+        keys.sort()
+        for key in keys:
+            f.write("%s %s\n"%(key,str(self.parameters[key])))
+        f.close()
+
+    def loadparameters(self,filename):
+        """
+        Load parameters from a file
+        """
+        lines = open(filename,"r").readlines()
+        for line in lines:
+            try:
+                [name, value] = line.split(" ") 
+                self.parameters[name]=value
+            except:
+                print "Failed to read:",line
+        self.dumbtypecheck()
+
+    def dumbtypecheck(self):
+        """
+        Eventually parameter types (and units and fixed/varied to be
+        specifieable
+        For now it just tries to coerce to float, then does nothing
+        """
+        for name, value in self.parameters.items():
+            if type(value) == type("string"):
+                try:
+                    vf = float(value)
+                except:
+                    # it really is a string
+                    self.parameters[name] = value
+                    continue
+                # here if float worked
+                try:
+                    vi = int(value)
+                except:
+                    # it really is a float
+                    self.parameters[name] = vf
+                    continue
+                    
+                # here if float and int worked
+                if abs(vi - vf) < 1e-9:
+                    # use int
+                    self.parameters[name] = vi
+                    continue
+                else:
+                    self.parameters[name] = vf
+                    continue
+            else:
+                # int/float preserve type
+                self.parameters[name] = value
+                
---
+++ server/src/build/scripts-2.4/guicommand.py
@@ -0,0 +1,104 @@
+
+
+# ImageD11_v0.4 Software for beamline ID11
+# Copyright (C) 2005  Jon Wright
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Interface between Tkinter gui and the actual useful code.
+
+There should be no scientific algorithms (eventually) on
+the gui side of this class.
+
+This class will eventually offer macro recording capability.
+"""
+
+
+# Things to offer from gui
+from Imaged11Functions import peakmerge, indexing, transform
+
+
+class guicommand:
+    """
+    Keeps a log of all commands issued - separates gui code from
+    algorithmical code
+    """
+    def __init__(self):
+        self.objects = { "peakmerger" : peakmerge.peakmerger(),
+                         "transformer": transform.transformer(),
+                         "indexer"    : indexing.indexer()
+                         }
+
+        self.commandscript = """
+  from ImageD11 import peakmerger, indexing, transform
+  mypeakmerger = peakmerger.peakmerger()
+  mytransformer = transform.transformer()
+  myindexer = indexing.indexer()
+  """
+
+    def execute(self,object,command,*args,**kwds):
+        """
+        Pass in object as string [peakmerger|transformer|indexer]
+        Pass in command as string, getattr(command) will be used
+        Returns the return value of the function....
+
+        TODO : change this interface???
+             eg : works - returns True
+                          you look for self.lastreturned
+                  fails - returns False
+                          you look for self.lasttraceback
+        """
+        if object not in self.objects.keys():
+            raise Exception("ERROR! Unknown command object")
+        o = self.objects[object]
+        func = getattr(o,command)
+        try:
+            ran = "my%s.%s("%(object,command)
+            addedcomma = ""
+            for a in args:
+                ran="%s %s %s"%(ran,addedcomma,repr(a))
+                addedcomma=","
+            for k,v in kwds.items():
+                ran="%s %s %s=%s "%(ran,addedcomma,k,v)
+                addedcomma=","
+            ran+=" )"
+            ret = func(*args, **kwds)
+
+        except:
+            print self
+            print object
+            print command
+            print func
+            print args
+            print kwds
+            raise
+        print "Ran:",ran
+        self.commandscript+=ran
+        return ret
+
+    def getdata(self,object,name):
+        """
+        Allows access to "live" data in the objects wrapped
+
+        By passing references back you can circumvent the
+        cleanliness of the interface. Please dont.
+
+        Returns object.name
+        """
+        if object not in self.objects.keys():
+            raise Exception("ERROR! Unknown command object")
+        attribute = getattr(self.objects[object],name)
+        return attribute
