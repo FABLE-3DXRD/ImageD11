120170e59c2228e200b251a2c8298034b3e9daa1
Jonathan Wright <jpwright@users.sf.net>
2006-11-22 21:21:18 +0000

    Update docs - trying to prepare incremental release

--- ImageD11/doc/makedocs.py
+++ ImageD11/doc/makedocs.py
@@ -2,6 +2,7 @@
 
 import pydoc, os, ImageD11,inspect
 pydoc.writedoc("ImageD11")
+os.system("copy ImageD11.html index.html")
 done=[None]
 for file in os.listdir(ImageD11.__path__[0]):
     path = os.path.join(ImageD11.__path__[0], file)
