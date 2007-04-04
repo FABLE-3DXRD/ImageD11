3cc747d75320183b1772f9f6ab4ee20e58d79cb7
Jonathan Wright <jpwright@users.sf.net>
2007-04-04 06:19:57 +0000

    pylint scores

--- ImageD11/__init__.py
+++ ImageD11/__init__.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
-
-__version__="0.8.1"
-__author__='Jon Wright',
-__author_email__='wright@esrf.fr'
+ 
+__version__ = "0.8.1"
+__author__ = 'Jon Wright',
+__author_email__ = 'wright@esrf.fr'
