85ac62ef46d8da4c0e0edb2646aec8b3a3f9bfa3
Jonathan Wright <jpwright@users.sf.net>
2007-04-04 06:21:47 +0000

    pylint scores

--- ImageD11/findreflections.py
+++ ImageD11/findreflections.py
@@ -10,29 +10,32 @@ def loadfiltered(filename):
     """
     Read in file containing filtered and merged peak positions
     """
-    f=open(filename,"r")
+    file_obj=open(filename,"r")
     #                   0123456789012
-    line=f.readline()
-    if line[0:12] !="# xc yc omega"[0:12]:
-        print line
-        raise Exception("Sorry That does not seem to be a filter peaks file, output from the peaksearching menu option")
+    thisline = file_obj.readline()
+    if thisline[0:12] != "# xc yc omega"[0:12]:
+        print thisline
+        raise Exception("Sorry That does not seem to be a filter peaks file,"+\
+                        " output from the peaksearching menu option")
     # titles = line.replace("#","").split()
     bigarray=[]
-    for line in f.readlines():
-        v=[float(z) for z in line.split()]
-        bigarray.append(v)
-    f.close()
+    for thisline in file_obj.readlines():
+        values = [float(z) for z in thisline.split()]
+        bigarray.append(values)
+    file_obj.close()
     return bigarray
 
 import math
-def findclosest(x,y,omega,data):
-    dmin=20.
-    vmin=None
+
+def findclosest(xin, yin, omega_in ,data):
+    """ Finds the closest values in data to x,y,omega"""
+    dmin = 20.
+    vmin = None
     for values in data:
         xp,yp,op = values[0:3]
-        dist = math.sqrt((x-xp)*(x-xp)+(y-yp)*(y-yp))
-        if dist<dmin and abs(omega-op)<2.:
-            vmin=values
+        dist = math.sqrt((xin-xp)*(xin-xp)+(yin-yp)*(yin-yp))
+        if dist < dmin and abs(omega_in - op) < 2.:
+            vmin = values
             dmin = dist
     return vmin
 
@@ -61,12 +64,12 @@ if __name__=="__main__":
         x,y,omega =  [float(v) for v in items[7:10]]
         print h,k,l,x,y,omega
         for scan in scans:
-           line = findclosest(x,y,omega,scandata[scan])
-           results.write("%f %s "%(loads[scan],scan))
-           if line is None:
-               results.write(" Nothing found within tolerance ")
-           else:
-               for item in line:
-                   results.write("%6g "%(item))
-           results.write("\n")
+            line = findclosest(x,y,omega,scandata[scan])
+            results.write("%f %s "%(loads[scan],scan))
+            if line is None:
+                results.write(" Nothing found within tolerance ")
+            else:
+                for item in line:
+                    results.write("%6g "%(item))
+            results.write("\n")
         results.write("\n\n\n")
