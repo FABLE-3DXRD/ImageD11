9f8c2b4df432b2a8b31e6d5e648ce1c2e9a8a638
Jonathan Wright <jpwright@users.sf.net>
2007-04-13 23:47:29 +0000

    build and test, assuming workspace is on jons laptop

--- winmake.bat
+++ winmake.bat
@@ -1,4 +1,15 @@
-setup.py build --compiler=mingw32 install sdist --formats=gztar,zip bdist_wininst
+
+cd "\documents and settings\wright\eclipse_new\imaged11"
+setup.py build --compiler=mingw32 install 
+cd test
+cd demo
+test.py
+cd ..
+cd ..
+
+goto END 
+
+setup.py build --comiler=mingw32 sdist --formats=gztar,zip bdist_wininst
 
 
 REM Py2exe is not quite automatic. Doubtless this is version and computer specific...
@@ -11,4 +22,6 @@ copy c:\python24\lib\site-packages\OpenGl\Tk\win32-tk8.4\* dist\
 cd dist
 del /q _na* _ns* ssl.pyd
 
+
+:END
 
