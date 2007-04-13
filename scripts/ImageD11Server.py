a0f12509dd302f21744695b0f80f2bc46b074a60
Jonathan Wright <jpwright@users.sf.net>
2007-04-13 23:46:18 +0000

    logging looking more hopeful now.

--- scripts/ImageD11Server.py
+++ scripts/ImageD11Server.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python2.4
-
+#!C:\Python24\python.exe
+"""
+Docstring
 # By  Jaroslaw Butanowicz
 # summer 2006
 # modified by JPW from Activestate python recipies
 #  - limited to localhost access for security
 #  
-
+"""
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-import sys, logging
+import sys, logging, threading
 
 class Server(SimpleXMLRPCServer):
     """
@@ -18,59 +19,39 @@ class Server(SimpleXMLRPCServer):
     """
     accessList=('127.0.0.1')
 
-    def __init__(self,*args,**kwds):
-        SimpleXMLRPCServer.__init__(self,*args,**kwds)
+    def __init__(self, *args, **kwds):
+        SimpleXMLRPCServer.__init__(self, *args, **kwds)
+        self.quit = False
         
     def server_bind(self):
+        """
+        Could be used to reuse same port (ouch)
+        """
         # see unix $ man 2 setsockopt
         # Don't want to rebind I think - prefer to crash out if running
         # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         SimpleXMLRPCServer.server_bind(self)
 
-    def verify_request(self,request, client_address):
+    def verify_request(self, request, client_address):
         """
         Not quite security...
         """
         if client_address[0] in self.accessList:
             return 1
         else:
+            logging.debug("refused request from %s "% (
+                           str(client_address)))
             return 0
         
-    def do_POST(self):
-        clientIP, port = self.client_address
-	    # Log client IP and Port
-        logging.info('Client IP: %s - Port: %s' % (clientIP, port))
-        try:
-            # get arguments
-            data = self.rfile.read(int(self.headers["content-length"]))
-            # Log client request
-            logging.info('Client request: \n%s\n' % data)
+#    def do_POST(self):
+#        """
+#        Removed - dont believe this is ever called by XMLRPC server
+#        """
         
-            response = self.server._marshaled_dispatch(
-                    data, getattr(self, '_dispatch', None)
-                )
-	        # Log server response
-            logging.info('Server response: \n%s\n' % response)
-        
-        except: # This should only happen if the module is buggy
-            # internal error, report as HTTP server error
-            self.send_response(500)
-            self.end_headers()
-        else:
-            # got a valid XML RPC response
-            self.send_response(200)
-            self.send_header("Content-type", "text/xml")
-            self.send_header("Content-length", str(len(response)))
-            self.end_headers()
-            self.wfile.write(response)
-
-            # shut down the connection
-            self.wfile.flush()
-            self.connection.shutdown(1)
-
     def serve_forever(self):
-        logging.debug("debugging serve_forever")
-        self.quit = 0
+        """ Mainloop of the server """
+        logging.debug("Server serve_forever called")
+        self.quit = False
         while not self.quit:
             self.handle_request()
             # try to help out the reads of stderr/stdout in the java gui
@@ -78,64 +59,68 @@ class Server(SimpleXMLRPCServer):
             sys.stderr.flush()
 
 
-
-
+class XMLRPCThread(threading.Thread):
+    """ Thread to run the server while we wait for control-c """
+    def __init__(self, port):
+        """ 
+        Some things appear to be thread local in this namespace here
+        This function will run the server in thread
+        """            
+        threading.Thread.__init__(self)
+        self.port = port
+        
+    def run(self):
+        """ The actual bit to spawn - server mainloop at end"""
+        server = Server( ("localhost", self.port ) , logRequests = 0 )
+        def shutdown():
+            """ stop the server """
+            logging.info( "ImageD11Server(): called shutdown()" ) 
+            server.quit = True
+        server.register_function(shutdown)
+                
+        def setLevel(level):
+            """ Set the logging level of the root console logger"""
+            logging.info( "ImageD11Server(): called setLevel "+str(level) ) 
+            rt = logging.getLogger('')
+            rt.setLevel(level)
+            
+        server.register_function(setLevel)
+    
+        from ImageD11 import guicommand
+        guicommander = guicommand.guicommand()            
+        server.register_instance(guicommander)
+    
+        #Go into the main listener loop
+        logging.info("Python Server is listening on port " + str(self.port))
+        server.serve_forever()
+    
+    
 
 def RunXMLRPCServer(port):
-        """Run the XMLRPCServer, but do so in a different thread.
-        The main thread simply sleeps so that it can respond to Ctrl+Break
-        http://mail.python.org/pipermail/python-list/2003-July/212751.html
-        """
-        def XMLRPCThread():
-            """ Things appear to be thread local in this namespace here
-            """        	
-            server = Server( ("localhost", port ), logRequests=0 )
-            def shutdown():
-                logging.info( "ImageD11Server(): called shutdown()" ) 
-                server.quit = 1
-            server.register_function(shutdown)
-            
-            import logging 
-            
-            # define a Handler which writes DEBUG messages or higher to the sys.stdout
-            console = logging.StreamHandler(sys.stdout)
-            console.setLevel(logging.DEBUG)
-            # set a format which is simpler for console use
-            formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-            # tell the handler to use this format
-            console.setFormatter(formatter)
-            # add the handler to the root logger
-            logging.getLogger('').addHandler(console)
-            
-            def setLevel(level):
-                logging.info( "ImageD11Server(): called setLevel "+str(level) ) 
-                console.setLevel(level)
-                
-            server.register_function(setLevel)
-
-            from ImageD11 import guicommand
-            guicommander = guicommand.guicommand()            
-            server.register_instance(guicommander)
+    """Run the XMLRPCServer, but do so in a different thread.
+    The main thread simply sleeps so that it can respond to Ctrl+Break
+    http://mail.python.org/pipermail/python-list/2003-July/212751.html
+    """
     
-            #Go into the main listener loop
-            logging.info("Listening on port "+str(port))
-            server.serve_forever()
-
-        import threading, signal, time
-        th = threading.Thread(target=XMLRPCThread)
-        th.setDaemon(1)
-        th.start()
-        # Make ctrl+break raise a KeyboardInterrupt exception.
-        try:
-            signal.signal(signal.SIGBREAK, signal.default_int_handler)
-        except:
-            print "signal.SIGBREAK not supported on this platform"
-        try:
-            while th.isAlive():
-                time.sleep(1)
-        except:
-            logging.info("Break pressed.")
-            sys.stdout.flush()
+    # real function starts here
+    import signal, time
+    # the lambda binds func(port) to func() having no args
+    # define a script/module level Handler which writes DEBUG messages or
+    # higher to the sys.stdout
+    th = XMLRPCThread(port)
+    th.setDaemon(1)
+    th.start()
+    # Make ctrl+break raise a KeyboardInterrupt exception.
+    try:
+        signal.signal(signal.SIGBREAK, signal.default_int_handler)
+    except:
+        logging.debug("signal.SIGBREAK not supported on this platform")
+    try:
+        while th.isAlive():
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logging.info( "Break pressed." )
+        sys.stdout.flush()
         
 
 if __name__=="__main__":
@@ -147,14 +132,28 @@ if __name__=="__main__":
 #    logger.addHandler(hdlr)
 
 
-    import sys
-    port = 51234
+    use_this_port = 51234 # high randomish private number
     if len(sys.argv)>1:
         try:
-            port = int(sys.argv[1])
-        except:
-            print "usage: %s [portnumber]"%(sys.argv[0])
-    RunXMLRPCServer(port)
+            use_this_port = int(sys.argv[1])
+        except (ValueError, IndexError) :
+            print "usage: %s [portnumber]"% (sys.argv[0])
+            
+            
+    # Set up the logging stuff
+    console = logging.StreamHandler(sys.stdout)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(levelname)-8s : %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    console.setLevel(logging.DEBUG)
+    
+    root = logging.getLogger('')
+    root.addHandler(console)
+    root.setLevel(logging.DEBUG) # should we process everything...?
+
+    RunXMLRPCServer(use_this_port)
 
 
 
