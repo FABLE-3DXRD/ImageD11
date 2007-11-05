82fcfd62632c7f83127fdcc55c687fc10c01c330
Jonathan Wright <jpwright@users.sf.net>
2007-11-05 16:07:12 +0000

    refactored to use optparse for command line handling

--- scripts/filtergrain.py
+++ scripts/filtergrain.py
@@ -3,51 +3,135 @@
 
 #!/bliss/users/blissadm/python/bliss_python/suse82/bin/python
 
-from ImageD11 import refinegrains
+"""
+Utility script to pick out the peaks belonging to a certain
+grain
+
+Used to stabilise refinements
+"""
+
+from ImageD11 import refinegrains, indexing
 import numpy.oldnumeric as Numeric
-import sys
-fltfile = sys.argv[1] #"peaks.out_merge_t100"
-parfile = sys.argv[2]
-ubifile = sys.argv[3]
-outfile = sys.argv[4]
-notfile = sys.argv[5] # the stuff NOT indexed
+import sys, logging
 
-o = refinegrains.refinegrains(tolerance=0.02)
 
-o.loadparameters(parfile)
-o.readubis(ubifile)
-o.loadfiltered(fltfile)
-o.generate_grains()
-for tol in range(1,11):
-    o.tolerance = tol*0.01
-    print "tol",o.tolerance,
-    o.refineubis(quiet=False)
-o.tolerance = float(raw_input("Enter tolerance"))
-for i,gn in zip(range(len(o.grainnames)),o.grainnames):
-    print i,gn
-gn = o.grainnames[int(raw_input("select which grain"))]
-o.compute_gv(gn,fltfile)
-matrix = o.refine(o.grains[(gn,fltfile)].ubi)
-
-h=Numeric.matrixmultiply(matrix,Numeric.transpose(o.gv))
-hint=Numeric.floor(h+0.5).astype(Numeric.Int) # rounds down
-diff=h-hint
-drlv=Numeric.sqrt(Numeric.sum(diff*diff,0))
-
-print o.scandata[fltfile].shape
-out = open(outfile,"w")
-out.write("# xc yc omega npixels avg_intensity x_raw y_raw sigx sigy covxy\n")
-out2 = open(notfile,"w")
-out2.write("# xc yc omega npixels avg_intensity x_raw y_raw sigx sigy covxy\n")
-for i in range(o.scandata[fltfile].shape[0]):
-    if drlv[i] < o.tolerance:
-        for v in o.scandata[fltfile][i,:]:
-            out.write("%f "%(v))
-        out.write("\n")
+def filtergrain(options):
+    """
+    Filter a peaks file according to which peaks are indexed
+    """   
+    o = refinegrains.refinegrains(tolerance=0.1)
+    o.loadparameters(options.parfile)
+    o.readubis(options.ubifile)
+    o.loadfiltered(options.fltfile)
+    o.generate_grains()
+    if options.grain is None:
+        if len(o.grainnames) == 1:
+            gn = o.grainnames[0]
+        else:
+            for i,gn in zip(range(len(o.grainnames)),o.grainnames):
+                logging.info("Choose %d for grain %s"%(i, gn))
+            gn = o.grainnames[int(raw_input("select which grain "))]
     else:
-        for v in o.scandata[fltfile][i,:]:
-            out2.write("%f "%(v))
-        out2.write("\n")
-out.close()
-out2.close()
+        gn = o.grainnames[int(options.grain)]  
+    o.compute_gv(gn, options.fltfile)
+    if options.tol is None:
+        for tol in [0.01,0.025,0.05,0.1,0.15,0.25,0.5]:
+            o.tolerance = tol
+            logging.info("tol %f"%(o.tolerance))
+            o.refineubis(quiet=False, scoreonly=True)
+        o.tolerance = float(raw_input("Enter tolerance "))
+        options.tol = o.tolerance
+    else:
+        o.tolerance = options.tol
+    matrix = o.grains[(gn,options.fltfile)].ubi
+    drlv2 = indexing.calc_drlv2( matrix, o.gv )    
+    
+    logging.info("Total peaks before filtering %d"%
+                     o.scandata[options.fltfile].nrows)    
+    gotpks = o.scandata[options.fltfile].copy()
+    gotpks.filter(drlv2 < o.tolerance*o.tolerance)
+    gotpks.writefile(options.newfltfile)
+    logging.info("Peaks which were indexed %d written to %s"%(
+                gotpks.nrows, options.newfltfile))
+    # don't bother to copy here as we can overwrite
+    if options.notindexed is not None:
+        notpks = o.scandata[options.fltfile]
+        notpks.filter(drlv2 > o.tolerance)
+        notpks.writefile(options.notindexed)
+        logging.info("Peaks which were not indexed %d written to %s"%(
+                notpks.nrows, options.notindexed))
+    if options.newubifile is not None:
+        o.scandata[options.fltfile] = gotpks
+        matrix = o.refine(matrix,quiet=True)
+        indexing.write_ubi_file(options.newubifile,[matrix])
+        logging.info("Refined ubi in %s "%(
+                         options.newubifile))
+        
+if __name__=="__main__":
+    
+    console = logging.StreamHandler(sys.stdout)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(levelname)-8s : %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    console.setLevel(logging.DEBUG)
+    root = logging.getLogger('')
+    root.addHandler(console)
+    root.setLevel(logging.DEBUG) # should we process everything...?
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-p",  "--parfile", action="store",
+                      dest="parfile", type="string",
+                      help="Name of parameter file")
+    parser.add_option("-u",  "--ubifile", action="store",
+                      dest="ubifile", type="string",
+                      help="Name of ubi file")
+    parser.add_option("-U", "--newubifile", action="store",
+                      dest="newubifile", type="string",
+                      help="Name of new ubi file")
+    parser.add_option("-f",  "--fltfile", action="store",
+                      dest="fltfile", type="string",
+                      help="Name of flt file")
+    parser.add_option("-F",  "--newfltfile", action="store",
+                      dest="newfltfile", type="string",
+                      help="Name of new flt file")
+    parser.add_option("-N",  "--notindexed", action="store",
+                      dest="notindexed", type="string",
+                      help="Name of flt file for unindexed peaks")
+    parser.add_option("-t", "--tol", action="store",
+                      dest="tol", type="float",
+                      default = None,
+                      help="Tolerance to use in peak assignment")
+    parser.add_option("-g","--grain", action="store",
+                      dest = "grain", type="string", default=None,
+                      help = "Which grain to choose")
+    parser.description = """
+Filtergrain should choose the peaks from a filtered
+peaks output file according to those which are closest 
+to a particular grain
+    """
+    
+    options, args = parser.parse_args()
+    
+    if None in [options.parfile, 
+                options.ubifile, 
+                options.newfltfile,
+                options.fltfile]:
+        parser.print_help()
+        logging.error("You have not filled in all the required options")
+        import sys
+        sys.exit()
+        
+    try:
+        filtergrain(options)
+    except:
+        parser.print_help()
+        import traceback
+        logging.error("An error occurred, details follow")
+        traceback.print_exc()
+    
+
 
--- scripts/fitgrain.py
+++ scripts/fitgrain.py
@@ -1,37 +1,114 @@
 #!/usr/bin/python
 #! /bliss/users/blissadm/python/bliss_python/suse82/bin/python
 
-from ImageD11 import refinegrains
-import sys, os
 
-parfile = sys.argv[1]
-ubifile = sys.argv[2]
-fltfile = sys.argv[3]
-newpars = sys.argv[4]
+"""
+Wrapper script to refine a single grain using 
+all peaks in a dataset (so that hkl assignments
+are not a problem).
+"""
 
-#if os.path.exists(newpars):
-#    ok = raw_input("OK to overwrite %s ? [y/n]"%(newpars))
-#    if ok not in ["y","Y","Yes","yes","YES"]:
-#        sys.exit()
+from ImageD11 import refinegrains, indexing
+import logging, sys
 
-o = refinegrains.refinegrains(tolerance=0.5)
-o.loadparameters(parfile)
-o.readubis(ubifile)
-o.loadfiltered(fltfile)
-o.generate_grains()
-o.refineubis(quiet=False)
-o.parameterobj.varylist = [ "y_center","z_center",
-        "tilt_y","tilt_x","tilt_z","wedge",
-#        "wavelength",
-        "t_x","t_y","distance"]
+def fitgrain(options):
+    """
+    Fits grains to a dataset using all peaks within tol
+    """
+    o = refinegrains.refinegrains(tolerance = options.tol)
+    o.loadparameters(options.parfile)
+    o.readubis(options.ubifile)
+    o.loadfiltered(options.fltfile)
+    o.generate_grains()
+    o.refineubis(quiet = False)
+    print o.gv[:10,:]
+    o.parameterobj.varylist = options.varylist
+    for p in options.fixlist:
+        try:
+            o.parameterobj.varylist.remove(p)
+        except:
+            pass
+    logging.info("Varying " + str(o.parameterobj.varylist))
+    o.fit(maxiters = options.steps)
+    o.refineubis(quiet = False)
+    o.saveparameters(options.newparfile)
+    ul = [g.ubi for g in o.grains.values()]
+    indexing.write_ubi_file(options.newubifile, ul)
 
-o.fit(maxiters=1000)
-o.refineubis(quiet=False)
 
-o.saveparameters(newpars)
 
-#from matplotlib.pylab import plot, show
-#plot(o.drlv)
-#show()
-for g in o.grains.values():
-    print g.ubi
+
+if __name__=="__main__":
+    
+    console = logging.StreamHandler(sys.stdout)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(levelname)-8s : %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    console.setLevel(logging.DEBUG)
+    root = logging.getLogger('')
+    root.addHandler(console)
+    root.setLevel(logging.DEBUG) # should we process everything...?
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-p",  "--parfile", action="store",
+                      dest="parfile", type="string",
+                      help="Name of parameter file")
+    parser.add_option("-u",  "--ubifile", action="store",
+                      dest="ubifile", type="string",
+                      help="Name of ubi file")
+    parser.add_option("-f",  "--fltfile", action="store",
+                      dest="fltfile", type="string",
+                      help="Name of flt file")
+    parser.add_option("-P", "--newparfile", action="store",
+                      dest="newparfile", type="string",
+                      help="Name of new parameter file")
+    parser.add_option("-U", "--newubifile", action="store",
+                      dest="newubifile", type="string",
+                      help="Name of new ubi file")
+    parser.add_option("-v", "--vary", action="append",
+                      dest="varylist", type="string",
+                      default =     [ "y_center","z_center",
+                                     "tilt_y","tilt_x","tilt_z","wedge",
+                                     "t_x","t_y","distance"],
+                      help="Parameters to vary"  )
+    parser.add_option("-x", "--fiX", action="append",
+                      dest="fixlist", type="string",
+                      default =     [ "chi","t_z" ],
+                      help="Parameters to fix (overrides vary)")
+    parser.add_option("-t", "--tol", action="store",
+                      dest="tol", type="float",
+                      default =   0.5,
+                      help="Tolerance to use in peak assignment")
+    parser.add_option("-s", "--steps", action="store",
+                      dest="steps", type="int",
+                      default =   1000,
+                      help="Number of simplex iterations")
+    
+    parser.description = """
+Fitgrain should attempt to fit one or more grains to a dataset
+using the parameters specified on the command line.
+    """
+    
+    options, args = parser.parse_args()
+
+    for name in ["parfile" , "newparfile",
+                 "ubifile", "newubifile",
+                    "fltfile"]:
+        if getattr(options, name) is None:
+            parser.print_help()
+            logging.error("Missing option "+name)
+            import sys
+            sys.exit()
+        
+    try:
+        fitgrain(options)
+    except:
+        parser.print_help()
+        import traceback
+        logging.error("An error occurred, details follow")
+        traceback.print_exc()
+    
