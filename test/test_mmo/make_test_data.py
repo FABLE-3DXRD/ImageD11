f3e16bf7a60ab8919d354606dac8c480cab108d5
Jonathan Wright <jpwright@users.sf.net>
2008-03-04 18:31:25 +0000

    Now catches the bug reported by Stine

--- test/test_mmo/make_test_data.py
+++ test/test_mmo/make_test_data.py
@@ -1,37 +1,61 @@
-
-
+################################################# make_test_data.py
+#
+#  save as a .py file and run it from a command line via:
+#  python make_test_data.py
 """
 Generate a series of test images
 """
 import Image
-# Make an image
-im = Image.new( "L", ( 64, 99) ) 
-for i in [0, 1, 2, 8, 9]:  # 0 , 1 , 2
+# make a test image
+im = Image.new( "L", ( 264, 299) ) 
+for i in [0, 1,            8, 9]: 
     im.save("test%04d.tif"%(i))
 # Put a 2x3 peak at (23, 45) for images 3-7 in a 0-9 series
-im.putpixel( (23,45) , 128)
-im.putpixel( (23,46) , 128)
-im.putpixel( (24,45) , 128)
-im.putpixel( (24,46) , 128)
-im.putpixel( (23,47) , 128)
-im.putpixel( (24,47) , 128)
-for i in range(3, 8):  # 3, 4, 5, 6, 7
+
+# peak 1 - simple blob
+for p in [  (23,45),   (23,46), (23,47), 
+            (24,45),   (24,46), (24,47)]:
+    im.putpixel( p , 128)
+
+# peak 2 - two will merge to one
+for p in [  (123,45),          (123,47), 
+            (124,45),          (124,47)]:
+    im.putpixel( p , 129)
+
+# peak 3 - one becomes two
+for p in [  (23,145), (23,146), (23,147), 
+            (24,145), (24,146), (24,147)]:
+    im.putpixel( p , 127)
+
+for i in [2, 3, 4]: 
+    im.save("test%04d.tif"%(i))
+
+for p in  [  (123,46),  
+             (124,46)   ]:
+    im.putpixel( p , 129)
+
+# peak 3 - one becomes two
+for p in [   (23,146),  
+             (24,146)]:
+    im.putpixel( p , 0)
+
+
+for i in [5, 6, 7]: 
     im.save("test%04d.tif"%(i))
     
 import os
 from ImageD11.columnfile import columnfile
 
 def test(start, step):
-    os.remove("peaks_t1.flt")
     os.system("c:\python25\python c:\python25\scripts\peaksearch.py " +
           "-n test -F .tif -f 0 -l 9 -t 1.0 -D 0 " + 
           "-T %f -S %f >> testpksearch.log"%(start,step))
     c = columnfile("peaks_t1.flt")
     print "start",start,"step", step,"finds:",
-    print "Min_o",c.Min_o[0],"Max_o",c.Max_o[0],"omega",c.omega[0]
-
+    print "Min_o",c.Min_o,"Max_o",c.Max_o,"omega",c.omega
+    if len(c.Min_o) == 3:
+        print "I think it might be OK"
 test( -4, +1) 
 test( -40, +1) 
 test( 40, +1) 
-
 
