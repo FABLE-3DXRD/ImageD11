ef4bb7a58504a6ca95927c6609a2bd60af416c00
Jonathan Wright <jpwright@users.sf.net>
2008-06-13 12:33:41 +0000

    Tidy test cases

---
+++ test/quantix/testfitfilt.py
@@ -0,0 +1,38 @@
+
+import sys, os
+
+if sys.platform == "win32":
+    PYT = "c:\\python25\\python"
+
+if sys.platform == "linux2":
+    PYT = "fable.python"
+
+FILT= os.path.join("..","..","scripts","filtergrain.py")
+FIT = os.path.join("..","..","scripts","fitgrain.py")
+
+
+
+os.system("%s %s %s"%(PYT, FILT, 
+        "-f peaks.out_merge_t100 -u g3.ubi -F new.flt -p g3.pars -g 0 -t 0.15"))
+
+os.system("%s %s %s"%(PYT, FIT, 
+        "-u g3.ubi -f new.flt -p g3.pars -P new.pars -U new.ubi -s 100"))
+
+os.system("%s %s %s"%(PYT, FILT, 
+        "-f peaks.out_merge_t100 -u new.ubi -F new.flt -p new.pars -g 0 -t 0.1"))
+
+os.system("%s %s %s"%(PYT, FIT, 
+        "-u g3.ubi -f new.flt -p g3.pars -P new.pars -U new.ubi -s 100"))
+
+os.system("%s %s %s"%(PYT, FILT, 
+        "-f peaks.out_merge_t100 -u new.ubi -F new.flt -p new.pars -g 0 -t 0.05"))
+
+os.system("%s %s %s"%(PYT, FIT, 
+        "-u g3.ubi -f new.flt -p g3.pars -P new.pars -U new.ubi -s 100"))
+
+os.system("%s %s %s"%(PYT, FILT, 
+        "-f peaks.out_merge_t100 -u new.ubi -F new.flt -p new.pars -g 0 -t 0.025"))
+
+os.system("%s %s %s"%(PYT, FIT, 
+        "-u new.ubi -f new.flt -p new.pars -P new.pars -U new.ubi -s 100"))
+
