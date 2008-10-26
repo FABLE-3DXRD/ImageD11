4077c738443ac263792c864756bc9d994cce04d2
Jonathan Wright <jpwright@users.sf.net>
2008-10-26 10:44:50 +0000

    Make it a unit test

--- test/peaksearchtiftest/make_test_data.py
+++ test/peaksearchtiftest/make_test_data.py
@@ -1,42 +1,58 @@
 
-
 from fabio import tifimage
+from ImageD11 import columnfile
 import numpy as np
-import os
-
-FIRST=6
-LAST =16
-
-def setUp():
-    blank = np.zeros((24,24),np.uint16)
-    peak   = blank.copy()
-    peak[ 12:14, 15:17 ] = 42
-
-    # put a peak on every other image
-    for i in range(FIRST, LAST+1):
-        if i%2 == 0:
-            obj = tifimage.tifimage( peak, { } )
-        else:
-            obj = tifimage.tifimage( blank, { } )
-        obj.write( 'tiftest%04d.tif'%(i))
+import os, unittest
 
 
-def tearDown():
+class test_tifs(unittest.TestCase):
 
-    for i in range(FIRST, LAST+1):
-        os.remove( 'tiftest%04d.tif'%(i) ) 
+    FIRST=6
+    LAST =16
+    NPK  =0 
+
+    def setUp(self):
+        blank = np.zeros((24,24),np.uint16)
+        peak   = blank.copy()
+        peak[ 12:14, 15:17 ] = 42
+        
+        # put a peak on every other image
+        for i in range(self.FIRST, self.LAST+1):
+            if i%2 == 0:
+                obj = tifimage.tifimage( peak, { } )
+                self.NPK += 1
+                print i, self.NPK
+            else:
+                obj = tifimage.tifimage( blank, { } )
+            obj.write( 'tiftest%04d.tif'%(i))
+        
+        if os.path.exists('peaks_t5.spt'):
+            os.remove('peaks_t5.spt')
+        if os.path.exists('peaks_t5.flt'):
+            os.remove('peaks_t5.flt')
+
+    def tearDown(self):
+        return
+        # we'll leave the files around for now
+
+        for i in range(self.FIRST, self.LAST+1):
+            os.remove( 'tiftest%04d.tif'%(i) )
+        
 
 
-def testpeaksearch():
-    if os.path.exists('peaks_t5.spt'):
-        os.remove('peaks_t5.spt')
-    if os.path.exists('peaks_t5.flt'):
-        os.remove('peaks_t5.flt')
-    os.system('peaksearch.py -n tiftest -f %d -l %d -t 5 -F .tif -S 0.3 -T 11 -p Y --OmegaOverRide'%(FIRST, LAST))
+    def testpeaksearch(self):
+        os.system("peaksearch.py -n tiftest " + \
+                  " -t 5 -F .tif -S 0.3 " + \
+                  " -T 11 -p Y --OmegaOverRide " +\
+                  " -f %d -l %d"%(self.FIRST, self.LAST))
+        results = columnfile.columnfile( "peaks_t5.flt" )
+        self.assertEqual( results.nrows, self.NPK)
 
+
+        
+        
 if __name__ == '__main__':
-    setUp()
-    testpeaksearch()
+    unittest.main()
     
    
     
