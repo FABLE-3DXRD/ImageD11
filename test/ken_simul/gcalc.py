922cea5909a588cf59a7e9b3f41c1ed6b63c84fd
Jonathan Wright <jpwright@users.sf.net>
2008-10-30 08:05:39 +0000

    Trivial simulator for checking diffractometer spec

---
+++ test/ken_simul/gcalc.py
@@ -0,0 +1,37 @@
+from ImageD11.indexing import readubis
+from ImageD11.transform import uncompute_g_vectors
+from ImageD11 import unitcell
+import numpy as np
+
+ul = readubis("test.ubi")
+ui = [ np.linalg.inv(ubi) for ubi in ul ]
+
+r = range(-4,5)
+hkls = np.array([ (h,k,l) for h in r for k in r for l in r ])
+
+
+gcalc = [ np.dot( ub, hkls.T).T for ub in ui]
+
+# 30 keV
+energy = 30
+wvln = 12.3985 / energy
+k = 0
+for g, ubi in zip(gcalc, ul):
+    k +=1
+    print "# Grain",k, "\n# Energy",energy,"keV, wavelength %.5f"%(wvln)
+    tth, eta, omega = uncompute_g_vectors( g.T, wvln )
+    order = np.argsort(tth)
+    hkls = np.dot( ubi, g.T).T
+    print "#  h  k  l  tth  eta  omega"
+    for j in range(len(order)):
+        i = order[j]
+        for s in (0,1):
+            h,k,l = [int(h) for h in hkls[i]]
+            if tth[i]>0  and tth[i] < 20 and \
+                    abs(eta[s][i])< 45 and not unitcell.F(h,k,l):
+
+                
+                print (" %4d"*3 + " %7.2f"*3) % tuple([h,k,l,
+                        tth[i],
+                        eta[s][i], omega[s][i]])
+    print
