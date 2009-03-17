2ea7d72d51ae5b6bf784522fefef84db00f201a3
Jonathan Wright <jpwright@users.sf.net>
2009-03-17 10:25:40 +0000

    Added a convolver using fftw3f - timing is about 0.39 s per cycle versus 1.34 s for numpy

--- sandbox/rl.py
+++ sandbox/rl.py
@@ -1,6 +1,141 @@
 #!/usr/bin/env fable.python
 import numpy as np
 
+try:
+    import fftw3f
+    REAL = np.float32
+
+    class convolver(object):
+
+        plans = {}
+
+        def __init__(self, pointspread, dims):
+            """
+            dims = target dimensions
+            pointspread = pointspread function to use
+            """
+            self.pointspread = pointspread
+            self.dims = dims
+
+            self.N = self.dims[0] * self.dims[1]
+        
+            self.PSF = centre_psf( pointspread, dims )
+    
+            self.QPSF = self._do_fft( self.PSF, 'forward' )
+            self.eps = 1.0/65535
+
+        def release_plan(self, input):
+            """
+            release the memory from previous plans
+            """
+            if self.plans.has_key( input.ctypes.data ):
+                # no idea if it leaks elsewhere
+                del self.plans[ input.ctypes.data ]
+
+
+        def _do_fft( self, input, direct):
+            """
+            Does the fft using fftw3f
+            It caches the plan and result of the fft so that when you 
+            do it again it will be faster
+            This means you are limited to only using a few arrays before 
+            you exhaust the machines memory
+            see also release_plan
+            """
+            key = input.ctypes.data
+            if self.plans.has_key( key ):
+                output , plan = self.plans[key]
+                plan.execute()
+                return output
+            # we've never done an fft of this array
+            if len(self.plans.keys())>6:
+                print "fftw3 is making too much cache"
+                print "needs to be programmed to reuse the same arrays"
+            if direct == 'forward':
+                assert input.dtype == np.float32
+                output = np.zeros( input.shape, np.complex64)
+            else:
+                assert input.dtype == np.complex64
+                output = np.zeros( input.shape, np.float32 )
+            fp = fftw3f.Plan( 
+                    inarray = input, 
+                    outarray = output, 
+                    direction = direct, 
+                    flags = ['estimate'] )
+            fp.execute()
+            self.plans[ input.ctypes.data ] = output, fp
+            return output
+
+        def convolve(self, image):
+            """
+            Does a convolution
+            Currently using wraparound - which should probably be changed to clamp
+            """
+            assert image.shape == self.dims
+            qim = self._do_fft( image, 'forward' )
+            np.multiply( qim ,self.QPSF, qim )
+            out = self._do_fft( qim, 'backward' )
+            np.divide( out, self.N, out)
+            return out
+
+        def convolve_transp(self, image):
+            """
+            Does a convolution
+            Currently using wraparound - which should probably be changed to clamp
+            """
+            assert image.shape == self.dims
+            qim = self._do_fft( image, 'forward' )
+            np.multiply( qim, self.QPSF.conj(), qim)
+            out = self._do_fft( qim, 'backward' )
+            np.divide( out, self.N, out)
+            return out
+
+  #  raise ImportError()
+
+    print "Using fftw3f, should perform better"
+
+except ImportError:
+
+    print "Using numpy fft for convolution"
+    print "You might get better performance from fftw, why not try installing:"
+    print "http://prdownload.berlios.de/pyfftw/PyFFTW3-0.2.tar.gz"
+    REAL = np.float
+
+    class convolver(object):
+
+        def __init__(self, pointspread, dims):
+            """
+            dims = target dimensions
+            pointspread = pointspread function to use
+            """
+            self.pointspread = pointspread
+            self.dims = dims
+            
+            self.PSF = centre_psf( pointspread, dims )
+            self.QPSF = np.fft.rfft2( self.PSF )
+
+            self.eps = 1.0/65535
+
+        def convolve(self, image):
+            """
+            Does a convolution
+            Currently using wraparound - which should probably be changed to clamp
+            """
+            assert image.shape == self.dims
+            qim = np.fft.rfft2( image )
+            return np.fft.irfft2( qim * self.QPSF )
+
+        def convolve_transp(self, image):
+            """
+            Does a convolution
+            Currently using wraparound - which should probably be changed to clamp
+            """
+            assert image.shape == self.dims
+            qim = np.fft.rfft2( image )
+            return np.fft.irfft2( qim * self.QPSF.conj() )
+
+
+
 
 
 def centre_psf( psf_array, target_image_dims, DEBUG=False ):
@@ -12,15 +147,15 @@ def centre_psf( psf_array, target_image_dims, DEBUG=False ):
     """
     assert psf_array.shape[0] < target_image_dims[0]
     assert psf_array.shape[1] < target_image_dims[1]    
-    proj_0 = psf_array.sum(axis = 1, dtype=np.float)
+    proj_0 = psf_array.sum(axis = 1, dtype=REAL)
     n0 = len(proj_0)/2
     com_0 = n0 + (proj_0*np.arange(-n0, len(proj_0)-n0)).sum() / proj_0.sum()
     com_0 = int(com_0)
-    proj_1 = psf_array.sum(axis = 0, dtype=np.float)
+    proj_1 = psf_array.sum(axis = 0, dtype=REAL)
     n1 = len(proj_1)/2
     com_1 = n1 + (proj_1*np.arange(-n1, len(proj_1)-n1)).sum() / proj_1.sum()
     com_1 = int(com_1)
-    output = np.zeros( target_image_dims, np.float )
+    output = np.zeros( target_image_dims, REAL )
     # 
     # 4 corners:
     #
@@ -32,7 +167,7 @@ def centre_psf( psf_array, target_image_dims, DEBUG=False ):
         output[ -com_0:, :p1-com_1] = psf_array[:com_0, com_1:]
     if com_0 > 0 and com_1 > 0:
         output[ -com_0:, -com_1:] = psf_array[:com_0, :com_1]
-    return output/output.sum()
+    return (output/output.sum()).astype(REAL)
 
 def do_scale(image, scale, offset):
     """ scales and offsets an image """
@@ -56,44 +191,14 @@ def prescale(image, tmin = 1./65535, tmax = 1.0):
     scale = 1.0*(tmax - tmin)/(mx - mn)
     return do_scale( image, scale, offset), scale, offset
 
-class convolver(object):
 
-    def __init__(self, pointspread, dims):
-        """
-        dims = target dimensions
-        pointspread = pointspread function to use
-        """
-        self.pointspread = pointspread
-        self.dims = dims
-        
-        self.PSF = centre_psf( pointspread, dims )
-        self.QPSF = np.fft.rfft2( self.PSF )
 
-        self.eps = 1.0/65535
-
-    def convolve(self, image):
-        """
-        Does a convolution
-        Currently using wraparound - which should probably be changed to clamp
-        """
-        assert image.shape == self.dims
-        qim = np.fft.rfft2( image )
-        return np.fft.irfft2( qim * self.QPSF )
-
-    def convolve_transp(self, image):
-        """
-        Does a convolution
-        Currently using wraparound - which should probably be changed to clamp
-        """
-        assert image.shape == self.dims
-        qim = np.fft.rfft2( image )
-        return np.fft.irfft2( qim * self.QPSF.conj() )
 
 def score( obs, calc):
     """ See how well our model matches the data """
     diff = (obs-calc)
     d2 = (diff*diff).ravel()
-    print "score",np.sqrt(d2.mean())
+    print "score",np.sqrt(d2.mean()),
 
 class RichardsonLucy(object):
     def __init__(self, conv, debug=True):
@@ -110,21 +215,25 @@ class RichardsonLucy(object):
         assert image.shape == self.convolver.dims
         obs, scale, offset = prescale(image) # obs [0->1]
         perfect = obs.copy()
+        import time
+        calc = obs.copy()
         for i in range(niter):
+            start = time.time()
             if self.debug: print "RL: iter",i,
             calc = self.convolver.convolve( perfect )
             if self.debug: score( obs, calc )
-            factor = obs / calc
-            correction = self.convolver.convolve_transp(factor)
-            perfect = perfect * correction
+            np.divide( obs, calc, calc ) 
+            correction = self.convolver.convolve_transp( calc )
+            np.multiply( perfect, correction, perfect)
+            print "%.3f"%(time.time()-start)
         rescaled = undo_scale(perfect, scale, offset)
         return rescaled
 
 
 def gauss2d( dims, sig):
     """ 2D Gaussian """
-    x = np.outer( np.ones(dims[0], np.float), np.arange(dims[1]))-dims[1]/2
-    y = np.outer( np.arange(dims[0]), np.ones(dims[1], np.float))-dims[0]/2
+    x = np.outer( np.ones(dims[0], REAL), np.arange(dims[1]))-dims[1]/2
+    y = np.outer( np.arange(dims[0]), np.ones(dims[1], REAL))-dims[0]/2
     arg =  x * x + y * y
     return np.exp( -arg / sig / sig )
 
@@ -143,7 +252,7 @@ def dodeconv( filename_in,
     im_bg = openimage( filename_bg)
 
     # do background subtraction
-    im_in.data = im_in.data.astype(np.float32) - im_bg.data - float(extra_bg)
+    im_in.data = im_in.data.astype(REAL) - im_bg.data - float(extra_bg)
     im_in.data = np.where( im_in.data > 0 , im_in.data, 0 )
     # conv does the image convolution
     conv = convolver( pointspread_image, im_in.data.shape )
