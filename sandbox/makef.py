877fd69a96dba0b5702aa7bc0501c4046de8257f
Jonathan Wright <jpwright@users.sf.net>
2009-03-31 20:54:29 +0000

    have a look at minuit - shame about the interface

---
+++ sandbox/makef.py
@@ -0,0 +1,35 @@
+
+
+
+def one_item_func( grain, par ):
+    diff = grains[i] - par*42. - 1.
+    return diff*diff
+
+def make_sum_func( n ):
+    call = 'lambda p_'+', p_'.join([str(i) for i in range(n)]) 
+    call += ' : '
+    for i in range(n):
+       call +=  '+ one_item_func( %d, p_%d )'%(i,i)
+    return eval( call )
+
+f = make_sum_func(3)
+grains = {}
+for i in range(10):
+    grains[i] = 43
+
+import minuit
+m = minuit.Minuit( f )
+
+m.migrad()
+print m.values
+
+f = make_sum_func(4)
+grains = {}
+for i in range(4):
+    grains[i] = -3.
+
+print 'HELLO'
+m = minuit.Minuit( f )
+
+m.migrad()
+print m.values
