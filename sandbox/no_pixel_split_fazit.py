e54a058ad616cd126d01fee0c0ef76171ed40b51
Jonathan Wright <jpwright@users.sf.net>
2010-04-21 09:58:45 +0000

    adds cli

--- sandbox/no_pixel_split_fazit.py
+++ sandbox/no_pixel_split_fazit.py
@@ -1,82 +1,123 @@
 
 
-import sys, numpy
+import sys, numpy, time
 from ImageD11 import closest, blobcorrector
 from fabio.openimage import openimage
 from fabio.edfimage import edfimage
 
-
-dataim = openimage(sys.argv[1]).data
-dxim = openimage(sys.argv[2]).data
-dyim = openimage(sys.argv[3]).data
-
-
-
-
-if False:
-    # Super slow
-    outsum = numpy.zeros( dataim.shape, numpy.float32 )
-    outnp = numpy.zeros( dataim.shape, numpy.int32 )
-    for i in range(dataim.shape[0]):
-        print i,
-        sys.stdout.flush()
-        for j in range(dataim.shape[1]):
-            dest_i = i + dxim[i,j]
-            dest_j = j + dyim[i,j]
-            outsum[ dest_i, dest_j ] += dataim[i,j]
-            outnp[ dest_i, dest_j ]  += 1
-else:
-
-    outsum = numpy.ravel( numpy.zeros( dataim.shape, numpy.float32 ) )
-    outnp = numpy.ravel( numpy.zeros( dataim.shape, numpy.float32 ) )
-
-    # C code from rsv_mapper (not intended to be obfuscated)
-    o = blobcorrector.perfect( )
-    idealx, idealy = o.make_pixel_lut( dataim.shape )
-
-    destx, desty = idealx + dxim, idealy + dyim
+def get_options(parser):
+    """
+    Command line interface for making a mapping
+    Add our options to a parser object
+    """
+    parser = ImageD11_file_series.get_options( parser )
     
-    assert destx.min() >= 0
-    assert destx.max() < dataim.shape[1]
-    assert desty.min() >= 0
-    assert desty.max() < dataim.shape[1]
 
 
-    indices = numpy.ravel( destx ).astype( numpy.intp )
-    numpy.multiply( indices, dataim.shape[1], indices )
-    numpy.add( indices,
-               numpy.ravel( desty ).astype( numpy.intp ),
-               indices )
-
-    assert indices.min() >= 0
-    assert indices.max() < dataim.shape[0]*dataim.shape[1]
-
-    import time
+def main():
+    """
+    A CLI user interface
+    """
+    import sys, time, os, logging
     start = time.time()
 
-    closest.put_incr( outsum,
-                      indices,
-                      numpy.ravel(dataim).astype(numpy.float32) )
+    root = logging.getLogger('')
+    root.setLevel(logging.WARNING)
 
-    on = numpy.ones(len(outnp), numpy.float32) 
-    closest.put_incr( outnp,
-                      indices,
-                      on )
+    try:
+        from optparse import OptionParser
+        parser = OptionParser()
+        parser = get_options( parser )
+        options, args = parser.parse_args()
+    except SystemExit:
+        raise
+    except:
+        parser.print_help()
+        print "\nProblem with your options:"
+        raise
 
-    print "The actual rebin took around",time.time()-start,"seconds"
+    first_image = True
 
-    outsum = outsum.reshape( dataim.shape )
-    outnp = outnp.reshape( dataim.shape ).astype(numpy.int32)
+    imagefiles = ImageD11_file_series.get_series_from_options( options, args )
+
+    try:
+        for fim in imagefiles:
+
+            if first_image: # allocate volume, compute k etc
+
+                first_image = False
+
+                dataim = fim.data
+                dxim = openimage(options.dxim).data
+                dyim = openimage(options.dyim).data
+
+                outsum = numpy.ravel( numpy.zeros( dataim.shape,
+                                                   numpy.float32 ) )
+                outnp = numpy.ravel( numpy.zeros( dataim.shape,
+                                                  numpy.float32 ) )
+
+                e = edfimage()
 
 
-# Normalise
-mask = outnp == 0
-signal = outsum / ( outnp + mask )
-signal = signal*(1-mask) 
+                # C code from rsv_mapper (not intended to be obfuscated)
+                o = blobcorrector.perfect( )
+                idealx, idealy = o.make_pixel_lut( dataim.shape )
 
-e = edfimage()
-e.data=signal
-e.write( sys.argv[4], force_type=numpy.float32)
+                destx, desty = idealx + dxim, idealy + dyim
+    
+                assert destx.min() >= 0
+                assert destx.max() < dataim.shape[1]
+                assert desty.min() >= 0
+                assert desty.max() < dataim.shape[1]
+
+                imageshape = dataim.shape
+                
+                indices = numpy.ravel( destx ).astype( numpy.intp )
+                numpy.multiply( indices, dataim.shape[1], indices )
+                numpy.add( indices,
+                           numpy.ravel( desty ).astype( numpy.intp ),
+                           indices )
+
+                assert indices.min() >= 0
+                assert indices.max() < dataim.shape[0]*dataim.shape[1]
+                on = numpy.ones(len(outnp), numpy.float32)
+                
+                # Number of pixels and mask are constant
+                closest.put_incr( outnp,
+                                  indices,
+                                  on )
+
+                mask = outnp == 0
+                scalar = ( 1.0 - mask ) / ( outnp + mask )
+
+                flatshape = outsum.shape
+
+                # 
+            # ENDIF firstimage
+
+            start = time.time()
+
+            numpy.multiply( outsum, 0, outsum )
+            outsum.shape = flatshape
+            
+            closest.put_incr( outsum,
+                              indices,
+                              numpy.ravel(dataim).astype(numpy.float32) )
+            
+            outsum.shape = imageshape
+
+            print "The actual rebin took around",time.time()-start,"seconds"
+
+            # outsum = outsum.reshape( dataim.shape )
+            # outnp = outnp.reshape( dataim.shape ).astype(numpy.int32)
+
+
+            # Normalise
+            numpy.multiply( outsum, scalar, outsum )
+
+            e.data=outsum
+
+            e.write( , force_type=numpy.float32)
 
 
 
