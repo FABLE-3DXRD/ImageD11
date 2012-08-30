83e07be12b41389aadf4fc2afbff3f7357d8590b
Jonathan Wright <jpwright@users.sf.net>
2012-08-30 14:46:47 +0000

    write animated gif and allow user defined scale factor. not finished yet

--- scripts/plotImageD11map.py
+++ scripts/plotImageD11map.py
@@ -19,32 +19,48 @@ def getmedian(s):
         return 0
     return float(items[j+2])
             
-
-sf = 1.
+try:
+    outersf = float(sys.argv[3])
+except:
+    outersf = 1.0
 k = 0
+print "Scale factor is",outersf
 for g in gf:
     k+=1
     #print g.translation, g.ubi
     mapfile.write("\n\n")
     o = g.translation
-    sf = pow(getmedian(g.intensity_info),0.3333)
+    sf = pow(getmedian(g.intensity_info),0.3333)*outersf
     for u in g.ubi:
         dodot(o,k)
         dodot(o+u*sf,k)
     for u in g.ubi:
         dodot(o,k)
         dodot(o-u*sf,k)
-    dodot(o,k)
-    dodot(o+sf*(-g.ubi[0]-g.ubi[1]),k)
-    dodot(o,k)
-    dodot(o+sf*(g.ubi[0]+g.ubi[1]),k)
+#    dodot(o,k)
+#    dodot(o+sf*(-g.ubi[0]-g.ubi[1]),k)
+#    dodot(o,k)
+#    dodot(o+sf*(g.ubi[0]+g.ubi[1]),k)
 
 mapfile.close()
 open("gnuplot.in","w").write("""
 set ticslevel 0
-set palette rgbformulae 3,2,2 model HSV
+set title "Color proportional to number of peaks (position in file)"
+set palette model RGB
+set palette defined ( 0 "violet", 1 "blue", 2 "green", 3 "yellow", 4 "orange", 5 "red" )
+set view equal
+set view 75,0,1,1
+set terminal gif animate delay 10 loop 1 optimize size 1024,768
+set nokey
+set hidden3d
+set output "ImageD11map.gif"
 splot "%s" u 1:2:3:4 w l lw 2 lc pal z
-"""%(sys.argv[2]))
+"""%(sys.argv[2])+
+"".join(["set view 75,%d\n replot\n"%(i) for i in range(1,360,1)])
+                             )
+
+
+    
 os.system("gnuplot -background white gnuplot.in -")
 
     
