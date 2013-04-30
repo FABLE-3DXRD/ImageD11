f5e37d0f24e2b509aed050d96cf8574df0ccdb6c
Jette Oddershede <jetteodder@users.sf.net>
2013-04-30 12:47:24 +0000

    pars_2_sweeper.py made more useful

--- scripts/pars_2_sweeper.py
+++ scripts/pars_2_sweeper.py
@@ -3,6 +3,11 @@
 
 #!/bliss/users/blissadm/python/bliss_python/suse82/bin/python
 
+"""
+Adapted to print detector line required for grainsweeper.3D
+Jette Oddershede, DTU Physics, jeto@fysik.dtu.dk, April 2013
+"""
+
 from ImageD11 import transform, parameters
 
 import sys, numpy.oldnumeric as Numeric
@@ -10,15 +15,18 @@ import sys, numpy.oldnumeric as Numeric
 try:
     parfile = sys.argv[1]
 except:
-    print "Usage: %s parfile [v h]"
+    print "Usage: %s parfile [v h] mm"
     print "generates parameters for grainsweeper from parameters for ImageD11"
+    print "mm should be 1 if par in mm and 1e-3 if par in microns"
     sys.exit()
 try:
     v=float(sys.argv[2])
     h=float(sys.argv[3])
+    mm=float(sys.argv[4])
 except:
-    v=512.
-    h=1536/2.
+    v=1023.5
+    h=1023.5
+    mm=1
 
 print "Getting parameters from",parfile
 
@@ -40,3 +48,5 @@ print "central pixel position"
 print "detector vertical",v,"horizontal",h
 print "real space x, y, z = ", xyz
 
+print "\n"
+print "detector 2048 2048 %0.4f %0.4f %0.4f %0.4f %0.4f %0.4f %0.4f %0.4f stem format %i %i %i %i" %(mm*pars['y_size'],mm*pars['z_size'],mm*xyz[0,0],mm*xyz[0,1],mm*xyz[0,2],180*pars['tilt_x']/Numeric.pi,180*pars['tilt_y']/Numeric.pi,180*pars['tilt_z']/Numeric.pi,pars['o11'],pars['o12'],pars['o21'],pars['o22'])
