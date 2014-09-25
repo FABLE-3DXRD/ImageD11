f4a074173d754ed7e837ebc3da2feb8598925f5c
Jonathan Wright <jpwright@users.sf.net>
2014-09-25 22:35:57 +0000

    only the chi stuff fails now

--- test/test_uncomputegv.py
+++ test/test_uncomputegv.py
@@ -9,7 +9,7 @@ class test_uncomputegv( unittest.TestCase ):
     def setUp(self):
         self.tth = np.array( [60, 10, 15, 20,25, 1.9555],np.float)
         self.wvln = 0.5
-        self.eta = np.array( [90, 10,120,-20,340, -73 ],np.float)
+        self.eta = np.array( [20, 10,120,-20,340, -73 ],np.float)
         self.omega = np.array([60,90,180, 60,97,  131],np.float)
         self.np = len(self.tth)
 
@@ -45,20 +45,26 @@ class test_uncomputegv( unittest.TestCase ):
                 self.wvln, 
                 wedge = self.w,
                 chi = self.c )
-        #print tth
-        #print eta
-        #print omega
+#        print tth
+#        print eta
+#        print omega
+        print "#  w  c  i  tth  tth  eta  eta  omega  omega"
         for i in range(len(tth)):
-            print
-            print self.w, self.c,
-            print i,self.tth[i], tth[i],
-            best = np.argmin( np.array(eta)[:,i] - self.eta[i] )
-            print i,self.tth[i], tth[i],self.eta[i], eta[best][i],\
-            self.omega[i], omega[best][i]
+
+            print self.w, self.c, i,
+            best = np.argmin( np.abs(angmod(np.array(eta)[:,i] - self.eta[i] )))
+            deta = angmod(np.array(eta)[best,i] - self.eta[i] )
+            domega = angmod(np.array(omega)[best,i] - self.omega[i] )
+            print self.tth[i], tth[i],self.eta[i], eta[best][i],\
+            self.omega[i], omega[best][i],eta[1-best][i],omega[1-best][i]
+
             self.assertAlmostEqual( self.tth[i], tth[i], 6 )
-            self.assertAlmostEqual( self.eta[i], eta[best][i], 6 )
-            self.assertAlmostEqual( self.omega[i], omega[best][i], 6 )
-            
+            self.assertAlmostEqual( deta, 0.0, 6 )
+            self.assertAlmostEqual( domega, 0.0, 6 )
+
+def angmod(x):
+    return np.degrees(np.arctan2( np.sin(np.radians(x)), np.cos(np.radians(x)) ))
+
 if __name__ ==  "__main__":
     unittest.main()
 
