21ba8b4354e24154860397862138dc6f833b4d7a
Jonathan Wright <jpwright@users.sf.net>
2016-11-28 15:40:39 +0000

    simple ring fitting example

---
+++ sandbox/fitstrain.py
@@ -0,0 +1,69 @@
+
+
+
+import numpy as np, pylab as pl
+
+def fitstrain( surf, pkcen, pkwid ):
+    """
+    surf = 2D surface from pyFAI of intensity versus tth/eta
+    pkcen = where the peak is on axis 1
+    pkwid = how much roi to use
+    """
+    lo = max(0, int(pkcen - pkwid/2) )
+    hi = min(surf.shape[1], int(pkcen + pkwid/2) )
+    roi = surf[:,lo:hi]
+    bg = roi.min(axis=1)
+    roi = (roi.T-bg).T
+    sIx = (roi*np.arange(roi.shape[1])).sum(axis=1)
+    sI  = roi.sum(axis=1)
+    cen = sIx/( sI + (sI==0))
+    if 0:
+        print surf.shape, pkcen, pkwid, lo, hi
+        pl.subplot(221)
+        pl.imshow(np.log(roi),aspect='auto',interpolation='nearest')
+        pl.colorbar()
+        pl.subplot(222)
+        pl.title(cen)
+        pl.plot(cen)
+        pl.subplot(223)
+        pl.title("tth-I")
+        pl.plot(roi.sum(axis=0))
+        pl.subplot(224)
+        pl.title("sI")
+        pl.plot(sI)
+        pl.show()
+    return cen, sIx, sI, lo, hi
+
+def fitcen( cen, w, azi):
+    """
+    cen = centroid positions
+    w   = weights (intensity or otherwise)
+    azi = angle, degrees
+    
+    Fits to equation :
+      cen = cen0 + A*sin(azi) + B*cos(azi) + C*sin(azi*2) + D*cos(azi*2)
+    """
+    n = len(cen)
+    m = 5 # number of variables in equation to fit
+    razi = np.radians( azi )
+    g = np.zeros( (m, n) )
+    g[0] = np.ones(n)
+    g[1] = np.sin(razi)
+    g[2] = np.cos(razi)
+    g[3] = np.sin(2*razi)
+    g[4] = np.cos(2*razi)
+    rhs = np.zeros(m)
+    mat = np.zeros((m,m))
+    for i in range(m):
+        rhs[i] = (g[i]*cen*w).sum()
+        for j in range(i,m):
+            mat[i,j] = mat[j,i] = (g[i]*g[j]*w).sum()
+    #print mat
+    imat = np.linalg.inv(mat)
+    sol = np.dot( imat, rhs )
+    calc = np.dot( g.T, sol )
+    return sol, calc
+
+    
+
+    
---
+++ sandbox/strainprocess.py
@@ -0,0 +1,89 @@
+
+
+import pyFAI, fabio, glob, os, sys, numpy as np, pylab as pl
+
+
+import fitstrain
+
+HOME="/data/visitor/ma3114/id11/4B_mechanical_test"
+poni="/data/visitor/ma3114/id11/Calibration/CeO2_calibrant_scan2.poni"
+# spline="/data/visitor/ma3114/id11/Calibration/frelon21_mar16.spline"
+mask = fabio.open("/data/visitor/ma3114/id11/fit2d.msk").data
+
+NTTH = 3000
+NETA = 3600
+
+ai = pyFAI.load(poni)
+
+
+todo = glob.glob("scan*")
+#todo = ["scan_20161124_160720_",]
+todo.sort()
+
+
+
+#I,tth,eta = ai.integrate2d( np.ones(mask.shape), NTTH, NETA, mask=mask )
+#I.sort(axis=0)
+#inds = [I.shape[0]-np.searchsorted(0.1,col)/2 for col in I.T]
+
+# better to compute the tth vals from a unit cell and find them...
+PEAKS = [ (1015, 150),
+          (1141, 70 ),
+          (1437, 150),
+          (1617, 70 ),
+          (1760, 100),
+          (1896, 70 ),
+          (2033, 70 ) ]
+
+
+
+def fitpeaks( I, tth, eta):
+    fits=[]
+    for x0,xw in PEAKS:
+        cen, sIx, sI, lo, hi = fitstrain.fitstrain( I, x0, xw )
+        th = tth[lo:hi]
+        mytth = np.interp( cen, np.arange(len(th)), th )
+        sol, calc = fitstrain.fitcen( mytth, np.sqrt( sI ), eta )
+        err = np.abs(mytth - calc)
+        # remove outliers
+        cut = np.median(err)*3
+        w = np.where( err > cut, 0, np.sqrt(sI) )
+        sol, calc = fitstrain.fitcen( mytth, w, eta )
+        if 0:
+            #print cen, mytth[lo:hi]
+            pl.plot(mytth,"+")
+            pl.plot(calc)
+            pl.show()
+        fits.append(sol)
+    return fits
+
+def hdr(o):
+    cntrs = o.header['counter_mne'].split()
+    cntrs.index('admet1')
+    cntvs = o.header['counter_pos'].split()
+    return "  ".join(( cntvs[cntrs.index('admet1')], 
+                       cntvs[cntrs.index('admet2')] ))
+
+for direc in todo:
+    os.chdir(os.path.join(HOME,direc))
+    edfs = glob.glob("*.edf")
+    edfs.sort()
+    sumdata = np.zeros((2048,2048),np.float32)
+    print "# Doing folder",direc,
+    for e in edfs:
+        np.add( sumdata, fabio.open(e).data, sumdata)
+        print e,
+        sys.stdout.flush()
+    print
+    I, tth , eta = ai.integrate2d( sumdata, NTTH, NETA,
+                                   mask = mask,
+                                   polarization_factor=0.0,
+                                   unit="2th_deg",
+                            )
+    fits = fitpeaks( I , tth, eta )
+    print direc,hdr(fabio.open(e)),
+    for f in fits:
+        print (len(f)*"  %g")%tuple(f),"    ",
+    print
+#            I.sort( axis = 0 )
+            
