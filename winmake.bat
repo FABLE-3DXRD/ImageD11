23b9b8e180375a07f14b499b7092d9edd2f92ebc
Jon Wright <wright@esrf.fr>
2017-04-03 16:12:45 +0200

    spring cleaning

--- winmake.bat
+++ winmake.bat
@@ -1,21 +1,16 @@
 
-set SRC="\users\wright\programming\fable\imaged11"
-set PYT=c:\python27\python
+set SRC=%~dp0
+set PYT=python
 cd %SRC%
-%PYT% setup.py build install 
+%PYT% setup.py build --compiler=mingw32
+%PYT% setup.py build bdist_wheel
+cd dist
+pip install ImageD11-1.6.0-cp27-cp27m-win_amd64.whl --no-deps -U
 
-%PYT% setup.py build sdist --formats=gztar,zip bdist_wininst
 GOTO TEST
 
-REM Py2exe is not quite automatic. Doubtless this is version and computer specific...
-cd py2exe
-copy OpenGL.__init__.py c:\python24\lib\site-packages\OpenGl\__init__.py
-copy OpenGL.Tk.__init__.py c:\python24\lib\site-packages\OpenGl\Tk\__init__.py
-del /q dist build
-%PYT% py2exesetup.py py2exe
-copy c:\python24\lib\site-packages\OpenGl\Tk\win32-tk8.4\* dist\
-cd dist
-REM del /q _na* _ns* ssl.pyd
+
+
 
 :TEST
 
@@ -65,9 +60,6 @@ cd %SRC%/test/testcolfile2db
 %PYT% -c "import ImageD11; print ImageD11.__version__"
 
 %PYT% -V
-
-REM Icon added to ImageD11_gui.exe by using XN_resource editor
-REM http://www.wilsonc.demon.co.uk/delphi.htm
 
 cd %SRC%
 
