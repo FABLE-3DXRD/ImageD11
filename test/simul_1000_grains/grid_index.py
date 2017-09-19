6790484a0433ce6c5a9fd3b2e461351140727fff
Jon Wright <wright@esrf.fr>
2017-09-19 06:12:50 +0200

    more automation

--- test/simul_1000_grains/grid_index.py
+++ test/simul_1000_grains/grid_index.py
@@ -1,7 +1,7 @@
 
 from ImageD11 import peakmerge, indexing, transformer
-from ImageD11 import grain
-import sys, os, numpy as np
+from ImageD11 import grain, unitcell, refinegrains
+import sys, os, numpy as np, time, random
 
 mytransformer = transformer.transformer()
 #
@@ -9,31 +9,132 @@ mytransformer = transformer.transformer()
 #    
 mytransformer.loadfiltered( sys.argv[1] )
 mytransformer.loadfileparameters(  sys.argv[2] )
-
 tmp = sys.argv[3]
+DSTOL = 0.004
+OMEGAFLOAT = 0.13
 NPKS = 9
-try:
-    NUL = open("/dev/null","w")
-except:
-    NUL = open("NUL","w")
+TOLSEQ = [ 0.03, 0.015, 0.0075]
+SYMMETRY = "cubic"
+RING1 = [1,0]
+RING2 = [1,0]
+CONSINE_TOL = 0.002
 
-def doindex( gve, x, y, z):
+# grid to search
+translations = [(t_x, t_y, t_z) 
+        for t_x in range(-500, 501, 100)
+        for t_y in range(-500, 501, 100) 
+        for t_z in range(-500, 501, 100) ]
+# Cylinder: 
+# filter( lambda x,y,z : (x*x+y*y)< 500*500, translations )
+random.seed(42) # reproducible
+random.shuffle(translations)
+
+
+# printing or quiet:
+if 0:
+    NUL = open("/dev/null","w")
+else:
+    NUL = sys.stdout
+    
+# Get the unit cell
+UC = unitcell.unitcell_from_parameters( mytransformer.parameterobj )
+
+
+col = mytransformer.colfile
+if not "drlv2" in col.titles:
+    col.addcolumn( np.ones(col.nrows, np.float),
+                   "drlv2" )
+if not "labels" in col.titles:
+    col.addcolumn( np.ones(col.nrows, np.float)-2,
+                   "labels" )
+if not "sc" in col.titles:
+    assert "xc" in col.titles
+    col.addcolumn( col.xc.copy(), "sc")
+if not "fc" in col.titles:
+    assert "yc" in col.titles
+    col.addcolumn( col.yc.copy(), "fc")
+
+
+class tick:
+    def __init__(self):
+        self.last = time.time()
+        self.start = self.last
+    def tick(self, msg=None):
+        if msg is not None:
+            sys.stdout.write(msg)
+        now = time.time()
+        sys.stdout.write(" %.2f total %.2f /s\n"%(now-self.last, now-self.start))
+        self.last=now
+
+ticker = tick()
+
+
+def domap(  OmFloat,  OmSlop,
+            pars,
+            colfile,
+            grainsfile,
+            tolseq = [ 0.03, 0.02, 0.01],
+            symmetry = "triclinic"):
+    """mapping function - does what makemap.py does"""
+    global NPKS
+    ss = sys.stdout # turns off printing
+    for tol in tolseq:
+        sys.stdout = NUL
+        o = refinegrains.refinegrains( OmFloat = OmFloat, OmSlop = OmSlop,
+                                       tolerance = tol,
+                                       intensity_tth_range = (0,180),
+                                       )
+        o.parameterobj = pars
+        # o.loadfiltered ...
+        o.scannames = ["internal"]
+        o.scantitles = colfile.titles
+        o.scandata["internal"] = colfile
+        o.tolerance = tol
+        o.readubis( grainsfile )
+        if symmetry is not "triclinic":
+            o.makeuniq( symmetry )
+        o.generate_grains()
+        o.refinepositions()
+        gl = filter( lambda x: x.npks > NPKS, o.grains.values() )
+        sys.stdout = ss
+        if len(gl) == 0:
+            print "I killed all your grains!"
+            break
+        else:
+            print "Keeping",len(gl),"from",len(o.grains.values()),"grains with at least",NPKS,"peaks",tol
+            grain.write_grain_file( grainsfile ,  gl )
+    return len(gl)
+
+
+
+def doindex( gve, x, y, z, w):
     global NUL
     global tmp
-    global NPKS 
+    global NPKS
+    global UC
+    global TOLSEQ
+    global CONSINE_TOL
     ss = sys.stdout # turns of printing
     sys.stdout = NUL
-    myindexer = indexing.indexer()
-    myindexer.readgvfile( gve )
-    for ring1 in [1,0]:
-        for ring2 in [1,0]:
+    myindexer = indexing.indexer(
+        wavelength = w,
+        unitcell = UC,
+        gv = gve.T
+        )
+    myindexer.ds = np.sqrt( (gve * gve).sum(axis=0) )
+    myindexer.ga = np.zeros(len(myindexer.ds),np.int)-1 # Grain assignments
+    #   myindexer.readgvfile( gve )
+
+    for ring1 in RING1:
+        for ring2 in RING2:
             myindexer.parameterobj.set_parameters(  {
-                'ds_tol': 0.004, 
+                'ds_tol': DSTOL, 
                 'minpks': NPKS, 
                 'max_grains': 1000, 
-                'hkl_tol': 0.02, 
+                'hkl_tol': TOLSEQ[0], 
                 'ring_1': ring1,
-                'ring_2': ring2
+                'ring_2': ring2,
+                'cosine_tol' : CONSINE_TOL,
                 } )
             myindexer.loadpars( )
             myindexer.assigntorings( )
@@ -45,61 +146,65 @@ def doindex( gve, x, y, z):
     return len(grains)
 
     
-import random
-random.seed(42)
+### Computation starts here
 
-translations = [(t_x, t_y, t_z) 
-        for t_x in range(-500, 501, 100)
-        for t_y in range(-500, 501, 100) 
-        for t_z in range(-500, 501, 100) ]
 
-random.shuffle(translations)
-import time
-start = time.time()
-
-#os.system("cp %s %s.flt"%(sys.argv[1],tmp))
+os.system("cp %s %s.flt"%(sys.argv[1],tmp))
 
 first = True
+
+w =  mytransformer.parameterobj.get("wavelength")
+
+totalgrains = 0
+
+# Filter tth range :
+mytransformer.updateparameters( )
+mytransformer.parameterobj.set_parameters(  {
+    't_x':0.0, 't_y':0.0, 't_z':0.0
+} )
+tth , eta = mytransformer.compute_tth_eta( )
+mytransformer.colfile.filter( tth < 5 )
+
+
+
 
 for t_x, t_y, t_z in translations:           
     mytransformer.updateparameters( )
     mytransformer.parameterobj.set_parameters(  {
                   't_x':t_x, 't_y':t_y, 't_z':t_z
                   } )
-    tth, eta = mytransformer.compute_tth_eta( )
+    mytransformer.compute_tth_eta( )
     if first:
-        mytransformer.colfile.filter( tth < 5 )
-        mytransformer.colfile.writefile( "%s.flt"%(tmp) )
-        tth, eta = mytransformer.compute_tth_eta( )
         mytransformer.addcellpeaks( )
     mytransformer.computegv( )
-    mytransformer.savegv( tmp+".gve" )
+    #    mytransformer.savegv( tmp+".gve" )
+    gve = np.vstack(( mytransformer.colfile.gx,mytransformer.colfile.gy,mytransformer.colfile.gz ))
     if first:
         first=False
-    ng = doindex( tmp+".gve", t_x,t_y,t_z)
-    print t_x, t_y, t_z, ng,time.time()-start
+    ng = doindex(gve, t_x,t_y,t_z, w)
+    print "Position",t_x, t_y, t_z,"Grains", ng,
+    ticker.tick()
     if ng > 0:
-        ret = os.system("makemap.py -u %s.ubi -U %s.map -s cubic --omega_slop=0.13 "%(tmp,tmp) +
-                            "-f %s.flt  -t 0.02 -p %s  "%(
-                               tmp,sys.argv[2]))
-        if ret !=0 :
-            print "bad 1"
-            raise
-        ret = os.system("makemap.py -u %s.map -U %s.map -s cubic --omega_slop=0.13 "%(tmp,tmp) +
-                            "-f %s.flt  -t 0.01 -p %s  "%(
-                               tmp,sys.argv[2]))
-        if ret !=0 :
-            print "bad 1"
-            raise
-        os.system("cutgrains.py %s.map %s.map %d"%(tmp,tmp,NPKS))
-        ret = os.system("makemap.py -u %s.map -U %s.map -s cubic --omega_slop=0.13  "%(tmp,tmp) +
-                            "-f %s.flt -F %s.flt -t 0.01 -p %s "%(
-                                tmp,tmp,sys.argv[2]))
-        if ret !=0 :
-            print "bad 3"
-            raise
-        open("all%s.map"%(tmp),"a").write(open("%s.map"%(tmp)).read())
-        mytransformer.loadfiltered("%s.flt"%(tmp))
+        #ret = os.system("makemap.py -u %s.ubi -U %s.map -s cubic --omega_slop=0.13 "%(tmp,tmp) +
+        #                    "-f %s.flt  -t 0.02 -p %s  "%(
+        #                       tmp,sys.argv[2]))
+        nfit = domap( True,  OMEGAFLOAT,
+                      mytransformer.parameterobj ,
+                      mytransformer.colfile ,
+                      "%s.ubi"%(tmp),
+                      tolseq = TOLSEQ,
+                      symmetry = SYMMETRY)
+        if nfit > 0:
+            open("all%s.map"%(tmp),"a").write(open("%s.ubi"%(tmp)).read())
+            mytransformer.colfile.filter( mytransformer.colfile.labels < -0.1 )
+            totalgrains += nfit
+        ticker.tick()
+    if  mytransformer.colfile.nrows == 0:
+        print "All peaks indexed"
+        break
+    else:
+        print "still got",mytransformer.colfile.nrows," to index, found",totalgrains,"in total"
+            
         
 
 
--- test/simul_1000_grains/test_mappers.py
+++ test/simul_1000_grains/test_mappers.py
@@ -9,27 +9,40 @@ args = {
     }
 
 # 1: Run makemap.py
-if 0:
-    cmd1 = "makemap.py -f %(flt)s -p %(par)s -s cubic --omega_no_float --no_sort "%args
+cmd1 = "makemap.py -f %(flt)s -p %(par)s -s cubic --omega_no_float --no_sort "%args
+if 1:
     cmd2 = "-U allgrid_makemap.map -t 0.015 -u %(ubi)s"%args
     os.system(cmd1+cmd2)
     cmd2 = "-U allgrid_makemap.map -t 0.0075 -u allgrid_makemap.map"
     os.system(cmd1+cmd2)
+if 1:
     cmd2 = "-U allgrid_makemap.map -t 0.0075 -u allgrid_makemap.map"
-    os.system("time -o makemap.log "+cmd1+cmd2)
+    os.system(cmd1+cmd2)
     
 
 
-args['flt']= os.path.join("ideal.flt")
-if 0:
+    
+
+#args['flt']= os.path.join("ideal.flt")
+
+
+if 1:
     # 2: Run sandbox/fittrans using assigned grains
     cmd = "python ../../sandbox/fittrans.py %(flt)s.new %(par)s %(ubi)s allgrid_fittrans.map "%args
-    os.system("time -o fittrans.log "+ cmd)
-
+    os.system( cmd)
+    print cmd
+if 1:
     # 3: Run ImageD11/indexer using assigned grains
     cmd = "python ../../ImageD11/indexer.py %(par)s %(flt)s.new fit %(ubi)s allgrid_indexer.map"%args
-    os.system("time -o indexer.log " + cmd)
-
+    os.system(cmd)
+    print cmd
 # 4: Run sandbox/teo.py  using assigned grains
-cmd = "python ../../sandbox/teo.py %(flt)s.new %(par)s %(ubi)s allgrid_teo.map"%(args)
-os.system("time -o teo.log "+ cmd)
+if 1:
+    cmd = "python ../../sandbox/teo.py %(flt)s.new %(par)s %(ubi)s allgrid_teo.map"%(args)
+    os.system( cmd)
+
+
+if  not os.path.exists("ideal.map"):
+    os.system("python res2map.py Al1000/Al1000.ubi Al1000/Al1000.res ideal.map")
+
+    os.system("python compare.py")
