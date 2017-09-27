0fdedee678e92b250dc42333e6d1f0b7390f67be
Jon Wright <wright@esrf.fr>
2017-09-27 14:22:01 +0200

    missed a print

--- ImageD11/ImageD11_thread.py
+++ ImageD11/ImageD11_thread.py
@@ -44,5 +44,5 @@ class ImageD11_thread(threading.Thread):
     def ImageD11_stop_now(self):
         global stop_now
         if stop_now:
-            print "Got a stop in",self.myname
+            print( "Got a stop in",self.myname)
         return stop_now
