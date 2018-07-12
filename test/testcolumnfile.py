3547ebb842d7a0273b7303a19e08357bd077a56c
jonwright <wright@esrf.fr>
2018-07-12 12:38:57 +0200

    Add updateGeometry method

--- test/testcolumnfile.py
+++ test/testcolumnfile.py
@@ -5,6 +5,43 @@ import unittest
 
 from ImageD11 import columnfile
 
+class testgeom( unittest.TestCase ):
+    def setUp( self ):
+        with open("testgeom.flt","w") as f:
+            f.write("""
+# chi = 0.0
+# distance = 5881.14814327
+# o11 = 1
+# o12 = 0
+# o21 = 0
+# o22 = 1
+# omegasign = 1.0
+# t_x = 31.8613635188
+# t_y = -28.5519205159
+# t_z = 0.0
+# tilt_x = -0.00319993715326
+# tilt_y = -0.00227706383676
+# tilt_z = 0.0172853900689
+# wavelength = 0.2469981
+# wedge = 0.0761143411257
+# y_center = 1080.16471254
+# y_size = 1.5
+# z_center = 978.22872971
+# z_size = 1.5
+#  sc  fc  omega  Number_of_pixels
+20  30  0  1
+21  31  30  1
+22  32  60  1
+23  33  70  1
+24  34  80  1
+""")
+    def test1( self ):
+        c  = columnfile.columnfile("testgeom.flt")
+        c.updateGeometry( )
+
+
+
+
 class test1( unittest.TestCase ):
     def setUp( self ):
         f=open("test.col","w")
@@ -13,47 +50,66 @@ class test1( unittest.TestCase ):
 5 6 1 2
 0 4 2 3
 32 5 6 2
+33 7 9 0
 """)
         f.close()
+        
+    def testaddcol1( self ):
+        c = columnfile.columnfile( "test.col")
+        c.addcolumn( [5,4,1,2,0], 'alice' )
+        self.assertEqual( c.titles[-1] , 'alice')
+        self.assertEqual( list(c.alice) , [5,4,1,2,0])
+        self.assertEqual( c.ncols , 5)
+        self.assertEqual( c.nrows , 5)
 
+    def testaddcol2( self ):
+        c = columnfile.columnfile( "test.col")
+        c.addcolumn( [5,4,1,2,9], 'a' )
+        self.assertEqual( c.titles.index("a") , 1 )
+        self.assertEqual( list(c.a) , [5,4,1,2,9])
+        self.assertEqual( c.ncols , 4)
+        self.assertEqual( c.nrows , 5)
+
+        
     def test1(self):
         c  = columnfile.columnfile( "test.col" ) 
-        self.assertEqual( list(c.b), [ 2, 1, 2, 6] )
+        self.assertEqual( list(c.b), [ 2, 1, 2, 6, 9] )
         c.removerows( "b" , [1] )
-        self.assertEqual( c.nrows , 3 )
-        self.assertEqual( list(c.b) , [ 2, 2, 6] )
+        self.assertEqual( c.nrows , 4 )
+        self.assertEqual( list(c.b) , [ 2, 2, 6, 9] )
         
     def test2(self):
         c  = columnfile.columnfile( "test.col" ) 
-        assert list(c.b) == [ 2, 1, 2, 6]
+        assert list(c.b) == [ 2, 1, 2, 6, 9]
         c.removerows( "b" , [2] )
-        assert c.nrows == 2
-        assert list(c.b) == [ 1, 6]
+        assert c.nrows == 3
+        assert list(c.b) == [ 1, 6, 9]
 
     def test3(self):
         c  = columnfile.columnfile( "test.col" ) 
-        assert list(c.a) == [ 1, 6, 4, 5]
+        assert list(c.a) == [ 1, 6, 4, 5, 7]
+        assert list(c.b) == [ 2, 1, 2, 6, 9]
         c.removerows( "a" , [1] )
         c.removerows( "a" , [4] )
-        assert c.nrows == 2
-        assert list(c.b) == [ 1, 6]
+        assert c.nrows == 3
+        assert list(c.b) == [ 1, 6, 9]
 
     def test4(self):
         c  = columnfile.columnfile( "test.col" ) 
-        assert list(c.a) == [ 1, 6, 4, 5]
+        assert list(c.a) == [ 1, 6, 4, 5, 7]
         c.removerows( "a" , [1, 4] )
-        assert c.nrows == 2
-        assert list(c.b) == [ 1, 6]
+        assert c.nrows == 3
+        assert list(c.b) == [ 1, 6, 9]
 
     def testsort(self):
         c  = columnfile.columnfile( "test.col" ) 
         c.sortby('a')
-        assert( list(c.a.astype(int))  == [ 1, 4, 5, 6] )
+        assert( list(c.a.astype(int))  == [ 1, 4, 5, 6, 7] )
 
     def testreorder(self):
         c  = columnfile.columnfile( "test.col" ) 
-        c.reorder([3,2,1,0])
-        assert( list(c.a)  == [ 5., 4., 6., 1.] )
+        c.reorder([4,3,2,1,0])
+        assert( list(c.a)  == [ 7, 5, 4, 6, 1] )
 
     def testmissingfile(self):
         with self.assertRaises( (IOError, FileNotFoundError) ):
