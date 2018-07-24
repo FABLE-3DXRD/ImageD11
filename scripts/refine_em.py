155ed16927ea03ecb44dac66e7d3401b84ba4817
Jonathan Wright <wright@esrf.fr>
2018-07-24 21:31:35 +0200

    improve to use multiprocessing

--- scripts/refine_em.py
+++ scripts/refine_em.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 from ImageD11.columnfile  import columnfile
 from ImageD11.grain import read_grain_file, write_grain_file
-import sys, os
+import sys, os, multiprocessing
 
-c = columnfile( sys.argv[1] )
-g = read_grain_file( sys.argv[2] )
+try:
+    c = columnfile( sys.argv[1] )
+    g = read_grain_file( sys.argv[2] )
+    parfile = sys.argv[3]
+    cmds = []
+except:
+    print( "Usage: %s colfile.flt.new grains.map parameters.par  --omega_slop=1 etc"%(sys.argv[0]))
+    sys.exit()
+    
 
 for i in range(len(g)):
     #g[i].translation[2] = 0.0
@@ -13,8 +21,12 @@ for i in range(len(g)):
     d = c.copy()
     d.filter( d.labels == i )
     d.writefile("%d.flt"%(i))
+    cmd = "fitgrain.py -p %s -u %d.ubi -U %d.ubi -P %d.par -f %d.flt -x t_z"%(
+        parfile,i,i,i,i)
+    for extra_arg in sys.argv[4:]:
+        cmd += " "+extra_arg
+    cmds.append( cmd )
 
-    os.system( "fitgrain.py -p %s -u %d.ubi -U %d.ubi -P %d.par -f %d.flt -x t_z"%(sys.argv[3],i,i,i,i))
+p = multiprocessing.Pool( multiprocessing.cpu_count() )
+p.map( os.system, cmds )
 
-#    for j in range(3):
-#            os.system( "fitgrain.py -u %d.ubi -U %d.ubi -p %d.par -P %d.par -f %d.flt"%(i,i,i,i,i))
