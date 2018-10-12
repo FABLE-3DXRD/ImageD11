1b83acc99932be6156b1f3511cb7d6c78daf917a
Jon Wright <wright@esrf.fr>
2018-10-12 13:54:15 +0200

    tag beta

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ needed =[]#
 
 # See the distutils docs...
 setup(name='ImageD11',
-      version='1.9.0',
+      version='1.9.0-beta',
       author='Jon Wright',
       author_email='wright@esrf.fr',
       description='ImageD11',
