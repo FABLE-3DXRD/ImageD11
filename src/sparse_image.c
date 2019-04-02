f652e0cf32d47e21f8c51bd3290151174ab0feda
Jonathan Wright <wright@esrf.fr>
2019-04-02 17:10:32 +0200

    Sparse connected pixels : WIP, test fails

---
+++ src/sparse_image.c
@@ -0,0 +1,241 @@
+
+
+
+#include <math.h>   /* sqrt */
+#include <stdio.h>  /* printf */
+#include <stdlib.h>  /* abs(int) */
+#include <string.h> /* memset */
+
+#include "blobs.h"
+
+
+
+
+/**
+ * Go through the data to compute sum and sum2 for mean and std 
+ * Also find min and max. Uses openmp for reductions
+ * 
+ * @param img Input data array  (1D or 2D.ravel(), so sparse or dense)
+ * @param npx length of data array (contiguous)
+ * @param *minval minimum of the pixels
+ * @param *maxval maximum of the pixels
+ * @param *s1  Sum of all pixel
+ * @param *s2  Sum of pixel^2
+ */
+void array_stats( float img[], int npx,
+		  float *minval, float *maxval,
+		  float *mean, float *var){
+  int i;
+  float mini, maxi;
+  /* Use double to reduce rounding and subtraction errors */
+  double t, s1, s2, y0; 
+  
+  s1 = 0.;
+  s2 = 0.;
+  mini = img[0];
+  maxi = img[0];
+  y0 = img[0];  
+  
+#pragma omp parallel for private(t) reduction(min:mini), reduction(max:maxi) reduction(+:s1,s2)
+  for( i = 0; i < npx; i++ ){
+    t = img[i] - y0;
+    s1 = s1 + t;
+    s2 = s2 + t * t;
+    if( img[i] < mini ){
+      mini = img[i];
+    }
+    if( img[i] > maxi ){
+      maxi = img[i];
+    }
+  }
+  /* results */
+  *mean = s1 / npx + y0;
+  *var  = (s2 - (s1*s1/npx))/npx;
+  *minval = mini;
+  *maxval = maxi;
+}
+
+
+/**
+ * Go through the data to compute a histogram of the values
+ * Previous call of array_stats would help to set up this call 
+ *  compare to np.bincount - this does not gain much
+ *  better implementations can be done 
+ *
+ * @param img[]  Input data array  (1D or 2D.ravel(), so sparse or dense)
+ * @param npx    length of data array (contiguous)
+ * @param low    Lower edge of first bin
+ * @param high   Upper edge of last bin
+ * @param hist[] Histogram to be output
+ * @param nhist  Number of bins in the histogram
+ */
+void array_histogram( float img[],
+		      int npx,
+		      float low,
+		      float high,
+		      int32_t hist[],
+		      int nhist){
+  int i, ibin;
+  float ostep;
+  memset( hist, 0, nhist * sizeof( int32_t ) );
+  /* Compute the multiplier to get the bin numbers */
+  ostep = nhist/(high - low);
+  for( i=0; i<npx; i++){
+    ibin = (int) floor( (img[i] - low)*ostep );
+    /* clip into range at ends */
+    if(ibin < 0){ 
+      ibin = 0;
+    } 
+    if(ibin >= nhist ){
+      ibin=nhist-1;
+    }
+    hist[ibin] = hist[ibin] + 1; 
+  }
+}
+
+/* 
+ * https://fr.mathworks.com/help/matlab/ref/sparse.html
+ * S = sparse(i,j,v,m,n,nz)
+ *   i = array of index i
+ *   j = array of index j
+ *   v = array of values
+ *   S( i(k), j(k) ) == v(k)
+ *   m = dimension 0 of S
+ *   n = dimension 1 of S
+ *   nz = number of non-zero values
+ *
+ * https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.coo_matrix.html
+ *  scipy.sparse.coo_matrix(arg1, shape=None, dtype=None, copy=False)
+ *   arg1 = ( v, i, j ) as above
+ *   shape = (m, n)     as above
+ *   dtype = float/double/int, etc
+ */
+
+/**
+ * Takes a pair of i,j index arrays and checks if they are sorted
+ *
+ *  @param i, j index arrays
+ *  @param nnz dimension of i, j
+ *  returns 0 for all OK
+ *          k for first non-sorted element
+ *         -k for first duplicate
+ */
+int sparse_is_sorted( uint16_t i[],
+		      uint16_t j[],
+		      int nnz,
+		      int verbose ){
+  int k, i0, j0;
+  i0 = i[0];
+  j0 = j[0];
+  if(verbose){printf("\n\nin sps : %d %d\n",nnz,verbose);}
+  for( k=1 ; k<nnz ; k++){
+    if(verbose) printf("k %d i0 %d j0 %d \n",k,i0,j0);
+    if( i[k] < i0 ) {
+      if(verbose) printf("k %d i[k] %d returning\n",k,i[k]);
+      return k;  /* Not sorted */
+    }
+    if( i[k] > i0 ) { /* New row */
+      i0 = i[k]; 
+      j0 = j[k];
+    } else {
+      if( j[k] < j0 ) {
+	if(verbose) printf("k %d j[k] %d returning\n",k,j[k]);
+	return k;  /* Not sorted */
+      }
+      if( j[k] == j0 ){
+	if(verbose) printf("k %d j[k] %d returning\n",k,j[k]);
+	return -k; /* Duplicate */
+      }
+      j0 = j[k];
+    }
+  }
+  return 0;
+}
+
+
+/** 
+ * Connected pixels
+ *
+ */
+
+int sparse_connectedpixels( float v[],
+			    uint16_t i[],
+			    uint16_t j[],
+			    int nnz,
+			    float threshold,
+			    int32_t labels[]  /* nnz */
+			    ){
+  int k, p, pp;
+  int32_t *S, *T, np;
+  /* Read k = kurrent
+          p = prev */
+  k = sparse_is_sorted( i, j, nnz, 0 );
+  if( k!= 0){
+    /* BAD */
+    k = sparse_is_sorted( i, j, nnz, 1 );
+    return k;
+  } else {
+    //printf( "OK, k=%d nnz=%d\n",k, nnz);
+  }
+  pp=0;
+  p=0;
+  memset( labels, 0, nnz*sizeof( int32_t ));
+  S = dset_initialise( nnz+2 );
+
+  for( k=0; k<nnz; k++){
+    //printf( "k %d, v[k] %f",k, v[k]);
+    labels[k] = 0;
+    if( v[k] > threshold ){
+      /* Decide on label for this one ... 
+       *
+       * 4 neighbors : k-1 is prev
+       */
+        
+        p = k-1; // previous pixel
+	if( (i[p] ==  i[k]   )   &&
+	    (j[p] == (j[k]-1))   &&
+	    ( labels[p] > 0 ) ) {
+	  match(&labels[k], &labels[p], S);
+	}
+	/* pp should be in row above, before j-1 */
+	while( (pp<k) && ((i[k]-i[pp]) > 1) ) pp++;
+	/* out if nothing on row above */
+	if( i[pp] == i[k] ) {
+	  if( labels[k] == 0){
+	    S = dset_new( &S, &labels[k] );
+	    continue;
+	  }
+	}
+	/* Now bring pp to j[k]-1 */
+	while( (j[k]-j[pp]) > 1 ) pp++;
+	/* Now look for the 3 in row above */
+	p = pp;
+	while( (j[p] - j[k]) < 2){
+	  if( (  i[p] == (i[k]-1))   &&   // same row
+	      (abs(j[p]-j[k]) < 2) ){     // col is +/- 1
+	    if( labels[p] > 0) {
+	      // Union p, k
+	      match( &labels[k], &labels[p], S);
+	    }
+	  }
+	  p++;
+	}
+	if( labels[k] == 0) dset_new( &S, &labels[k]);
+    }
+  } // end loop over data
+  T = dset_compress( &S, &np );
+  // renumber labels
+  for( k=0; k<nnz; k++){
+    if( labels[k] > 0 ){
+      if( T[labels[k]] == 0 ){
+	printf("Error in sparse_connectedpixels\n");
+      }
+      labels[k] = T[labels[k]];
+    }
+  }
+  free(S);
+  free(T);
+  return np;
+}
+
+
---
+++ test/test_sparse_image.py
@@ -0,0 +1,173 @@
+
+
+from ImageD11 import cImageD11
+import fabio
+import numpy as np
+import scipy.sparse
+import unittest
+import time
+
+class test_array_stats( unittest.TestCase ):
+    def test1( self ):
+        ar = np.arange( 6*4, dtype=np.float32 ).reshape( 6,4 )
+        mini, maxi, mean, var = cImageD11.array_stats( ar.ravel() )
+        self.assertTrue( np.allclose( 
+            (mini, maxi, mean, var),
+            (ar.min(), ar.max(), ar.mean(), ar.var()) ))
+    def test2( self ):
+        ar = np.ones( 6*4, dtype=np.float32 ).reshape( 6,4 )
+        mini, maxi, mean, var = cImageD11.array_stats( ar.ravel() )
+        self.assertTrue( np.allclose( 
+            (mini, maxi, mean, var),
+            (ar.min(), ar.max(), ar.mean(), ar.var()) ))
+    def test3( self ):
+        ar = np.arange( 2048*2048, dtype=np.float32 ).reshape( 2048, 2048 )
+        start = time.time() 
+        mini, maxi, mean, var = cImageD11.array_stats( ar.ravel() )
+        endc = time.time()
+        check = ar.min(), ar.max(), ar.mean(), ar.var()
+        endn = time.time()
+        self.assertTrue( np.allclose( (mini, maxi, mean, var), check ) )
+        numpytime = endn - endc
+        ctime = endc - start
+        print("array_stats: %.3f ms vs %.3f ms, speedup %.1f"%(
+            1e3*ctime, 1e3*numpytime, numpytime/ctime))
+
+
+class test_array_histogram( unittest.TestCase ):
+    def test1(self):
+        N = 255
+        a = (np.random.random( (2048, 2048))*(N-0.1)).astype(np.float32)
+        start = time.time()
+        hc = np.zeros( N, dtype=np.int32 )
+        cImageD11.array_histogram( a.ravel(), 0., N, hc )
+        endc= time.time()
+        hn = np.bincount( a.ravel().astype(int) )
+        endn = time.time()
+        if( hc.shape[0] != hn.shape[0]):
+            print("shape error")
+            print( a.max())
+            print(hc)
+            print(hn)
+        self.assertTrue( (hc == hn).all() )
+        numpytime = endn - endc
+        ctime = endc - start
+        print("array_histogram: %.3f ms vs %.3f ms, speedup %.1f"%(
+            1e3*ctime, 1e3*numpytime, numpytime/ctime))
+
+
+class test_sparse_is_sorted( unittest.TestCase ):
+    def setUp( self ):
+        self.i = np.arange(10, dtype = np.uint16 )
+        self.j = np.arange(10, dtype = np.uint16 )
+    def testOK( self ):
+        ret = cImageD11.sparse_is_sorted( self.i, self.j)
+        self.assertEqual( ret, 0)
+    def testrpt( self ):
+        self.i[5]=self.i[4]
+        self.j[5]=self.j[4]
+        ret = cImageD11.sparse_is_sorted( self.i, self.j)
+        self.assertEqual( ret, -5)
+    def testbadi( self ):
+        self.i[5]=self.i[4]-1
+        ret = cImageD11.sparse_is_sorted( self.i, self.j)
+        self.assertEqual( ret, 5)
+    def testbadj( self ):
+        self.i[5]=self.i[4]
+        self.j[5]=self.j[4]-1
+        ret = cImageD11.sparse_is_sorted( self.i, self.j)
+        self.assertEqual( ret, 5)
+
+
+class test_sparse_connected_pixels( unittest.TestCase ):
+    def setUp( self ):
+        img ="""
+00110100000000000000000000000000000000000000000000000000
+00001000000000000000000000000000000000000000000000000000
+00000000001111000000111110000000000000000000000000000010
+00000000011001000001100011000000000000000000000000010010
+11000000000000000000000000000000000000000000000000001111
+00000000000000000000000000000000020000000000000000000000
+00000000000000000000000000000000020000000000000000000000
+00000000000000000000000000000000000000000000000000000000
+00000000000000000000000000000000000000000000000000000001
+00000000000000000001100000000000000000000000000000010001
+00000000000000000010011000000000000000000000000000001110
+00000000000000000100000100000000000000000000000000000000
+00000000000000001000200010000000000000000000000000000000
+00000000000000001000000100000000000000000000000000000000
+00000000000000000100001000000000000000000000000000000000
+00000000000000000011110000000000000000000000000000000000
+00000000000000000000000000000000000000000000000000000000
+00001100000000000000000000000000000000000000000000000000
+11111100000000000000000000000000000000000000000000000000
+00010000000000000000000100000000000000000000000000000000
+00000000000000000000000000000000000000000000000000000000
+00000000000000000000000010000000000000000000000100000000
+"""
+        self.a = np.array([ [int(c) for c in line ] for line in img.split() ],
+                          dtype=np.float32)
+        self.s = scipy.sparse.coo_matrix( self.a )
+        self.threshold=0.5
+        if 0:
+            print(self.s.data)
+            print(self.s.row)
+            print(self.s.col)
+        
+    def testOK( self ):
+        i = self.s.row.astype( np.uint16 )
+        j = self.s.col.astype( np.uint16 )
+        v = self.s.data
+        self.assertTrue( (self.s.todense() == self.a).all() )
+        dstart = time.time()
+        dl = np.zeros( self.a.shape, 'i' )
+        nd = cImageD11.connectedpixels(  self.a , dl, self.threshold )
+        dend = time.time()
+        sl = np.zeros( v.shape, 'i' )
+        ns = cImageD11.sparse_connectedpixels( v, i, j, self.threshold, sl)
+        send = time.time()
+        sld = scipy.sparse.coo_matrix( (sl, (i, j)), shape=self.a.shape)
+        testcase = (sld.todense() == dl).all()
+
+        densetime = dend - dstart
+        sparsetime = send-dend
+        print("sparse_connectedpixels: %.3f ms vs %.3f ms, speedup %.1f"%(
+            1e3*sparsetime, 1e3*densetime, densetime/sparsetime))
+
+        if ~testcase:
+            import pylab as pl
+            pl.ioff()
+            pl.figure()
+            pl.subplot(131)
+            pl.title("t=%f"%(self.threshold))
+            pl.imshow( dl, interpolation='nearest')
+            pl.colorbar()
+            pl.subplot(132)
+            pl.imshow( sld.todense(), interpolation='nearest')
+            pl.colorbar()
+            pl.subplot(133)
+            pl.imshow( sld.todense() - dl, interpolation='nearest')
+            pl.colorbar()
+            pl.show()
+        self.assertTrue( testcase )
+"""     
+class test_sparse_connected_pixels_overlapim1(test_sparse_connected_pixels):
+    def setUp(self):
+        im = fabio.open("testoverlaps0000.edf").data.astype( np.float32 )
+        self.a = np.where( im > 1000, im, 0 )
+        self.s = scipy.sparse.coo_matrix( self.a )
+        self.threshold = 1500.
+
+class test_sparse_connected_pixels_overlapim2(test_sparse_connected_pixels):
+    def setUp(self):
+        im = fabio.open("testoverlaps0000.edf").data.astype( np.float32 )[:128,:128]
+        self.a = np.where( im > 1000.5, im, 0 )
+        self.s = scipy.sparse.coo_matrix( self.a )
+        self.threshold = 1000.5
+"""     
+
+if __name__=="__main__":
+    unittest.main()
+
+
+#    cd .. && python setup.py build && cd test && PYTHONPATH=../build/lib.linux-x86_64-2.7 python test_sparse_image.py
