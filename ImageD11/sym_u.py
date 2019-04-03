4e76ee8d8b4ab37f8458d844a9692624afe33447
Jonathan Wright <wright@esrf.fr>
2019-04-03 10:01:42 +0200

    towards better symmetry

--- ImageD11/sym_u.py
+++ ImageD11/sym_u.py
@@ -22,12 +22,12 @@ def m_from_string(s):
         m.append(np.array(r)-t)
     return np.array(m)
 
-def fmt(c): 
+def fmt(c):
     if c == 1:
         return "+"
     if c == -1:
         return "-"
-    else: 
+    else:
         return "%f"%(c)
 
 def m_to_string(m):
@@ -47,8 +47,8 @@ def m_to_string(m):
             st.append(",")
     return "".join(st)
 
-            
-            
+
+
 
 
 
@@ -103,7 +103,7 @@ class group:
         global DEBUG
         if DEBUG:
             print("making new group")
-        new = True            
+        new = True
         while new:
             for a in self.group:
                 for b in self.group:
@@ -122,8 +122,67 @@ def generate_group(*args):
     return g
 
 
+# From International tables the "Laue classes" are:
+#
+#  -1  Triclinic
+# 2/m  Monoclinic
+# mmm  Orthorhombic
+# 4/m  or 4/mmm Tetragonal
+# -3 or -3/m Trigonal
+# 6/m or 6/mmm Hexagonal
+# m-3 or m-3m Cubic
+#
+#  Note: the Laue class is the symmetry of the reflection intensities assuming friedel's
+#        law is obeyed. e.g. these are the 32 point groups combined with inversion.
+#        Needed for peak intensities
+#
+# These groups are used for lattice permutations, they should preserve the handedness
+#  and so they DO NOT CONTAIN MIRROR PLANES
+#
+crystallographic_point_groups = """
+# index Crystal_system  Hermann-Mauguin	Order
+1   Triclinic	1	1
+2   Triclinic	-1	2
+3   Monoclinic	2	2
+4   Monoclinic	m	2
+5   Monoclinic	2/m	4
+6   Orthorhombic	222	4
+7   Orthorhombic    mm2	4
+8   Orthorhombic    mmm 8
+9   Tetragonal	4   4
+10  Tetragonal	-4   4
+11  Tetragonal	4/m   8
+12  Tetragonal	422   8
+13  Tetragonal	4mm   8
+14  Tetragonal	-42m   8
+15  Tetragonal	4/mmm   16
+16  Trigonal    3	3
+17  Trigonal    -3	3
+18  Trigonal    32	6
+19  Trigonal    3m  6
+20  Trigonal    -3m 12
+21  Hexagonal	6   6
+22  Hexagonal	-6	6
+23  Hexagonal	6/m 12
+24  Hexagonal	622 12
+25  Hexagonal	6mm 12
+26  Hexagonal	-62m 12
+27  Hexagonal	6/mmm   24
+28  Cubic	23  12
+29  Cubic   m-3  24
+30  Cubic   432   24
+31  Cubic   -43m 24
+32  Cubic   m3m  48"""
+
+proper_point_groups = """1 2 3 4 6 222 322 422 622 23 432""".split()
+assert len(proper_point_groups) == 11
+improper_centrosymmetrical_point_groups = """-1 2/m -3 4/m 6/m mmm -3m 4/mmm 6/mmm m-3 m3m""".split()
+assert len(improper_centrosymmetrical_point_groups) == 11
+improper_acentric_point_groups = """m -4 -6 mm2 3mm 4mm -42m 6mm -62m -43m""".split()
+assert len(improper_acentric_point_groups) == 10
 
 def cubic():
+    """ P32 """
     return generate_group( "z,x,y",  "-y,x,z" )
 
 def hexagonal():
@@ -143,7 +202,7 @@ def rhombohedralP():
 def tetragonal():
     """ P4 75"""
     return generate_group ( "-y,x,z", "-x,y,-z" )
-    
+
 def orthorhombic():
     """ P222 16 """
     return generate_group( "-x,-y,z", "-x,y,-z" )
@@ -190,7 +249,7 @@ def find_uniq_hkls( hkls, grp, func=hklmax):
             uniq[i] = np.where( msk , cand[i], uniq[i] )
         tmax = np.where( msk , t, tmax )
     return uniq
-        
+
 
 class trans_group(group):
     """
@@ -251,9 +310,9 @@ def test():
                                                              [ 0, 0,1]] ))
     print("testing1")
     for op in [ "x,y,z", "-y,x-y,z", "-y,x,z"]:
-        d = np.linalg.det(m_from_string(op)) 
+        d = np.linalg.det(m_from_string(op))
         assert d == 1.0, "Determinant = %f %s"%(d,op)
-    print("testing2")    
+    print("testing2")
     assert len(cubic().group) == 24, "not 24 ops found for cubic !"
     assert len(hexagonal().group) == 12 ,"not 12 ops found for hexagonal !"
     assert len(trigonal().group) == 6 ,"not 7 ops found for trigonal !"+\
@@ -265,8 +324,8 @@ def test():
         r = f().group
         assert len(r) == 2, " not 2 ops for monoclinic "
 
-    assert np.allclose( 
-        find_uniq_u( np.array( 
+    assert np.allclose(
+        find_uniq_u( np.array(
                 [[0,1,0],[-1,0,0],[0,0,1]]),cubic()),
                      np.identity(3) ), "Should easily get this unique choice"
 
@@ -294,7 +353,7 @@ def test():
     g2.additem([0.1, 0.45, 10])
     print(g2.group)
     DEBUG = False
-    
+
 
 def getgroup(s):
     """
@@ -307,11 +366,11 @@ def getgroup(s):
         import ImageD11.sym_u
         return getattr(ImageD11.sym_u, s)
 
-    
+
 
 if __name__=="__main__":
     test()
-    
+
 
     u = np.array([[ 0.71850787 , 0.69517833,  0.02176059],
                  [-0.62925889 , 0.66306714, -0.40543213],
@@ -319,6 +378,6 @@ if __name__=="__main__":
 
     find_uniq_u(u,cubic(),debug=0)
 
-    
 
-        
+
+
