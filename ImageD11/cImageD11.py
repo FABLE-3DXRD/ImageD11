d61583104f4068289a9e6eb0ca79c1ec6e82f452
Jon Wright <jonathan.wright@gmail.com>
2019-04-06 00:36:12 +0200

    fix py3 import

--- ImageD11/cImageD11.py
+++ ImageD11/cImageD11.py
@@ -4,18 +4,18 @@
 import platform, sys
 if platform.system() == "Linux":
     if " avx2 " in open("/proc/cpuinfo").read():
-        from cImageD11_avx2 import *
+        from .cImageD11_avx2 import *
         cpu = 'avx2'
     else:
-        from cImageD11_sse2 import *
+        from .cImageD11_sse2 import *
         cpu = 'sse2'
 
 elif platform.system() == "Windows":
     if sys.version_info[:2] > (3,4):
-        from cImageD11_avx2 import *
+        from .cImageD11_avx2 import *
         cpu = 'avx2'
     else:
-        from cImageD11_sse2 import *
+        from .cImageD11_sse2 import *
         cpu = 'sse2'
 else:
     
