82e2264c7b6efa2c42f9cb3db8fc8adcfaa17243
Jon Wright <jonathan.wright@gmail.com>
2019-07-04 19:35:41 +0200

    v1.9.3

--- ImageD11/__init__.py
+++ ImageD11/__init__.py
@@ -18,7 +18,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
  
-__version__ = "1.9.2"
+__version__ = "1.9.3"
 __author__ = 'Jon Wright',
 __author_email__ = 'wright@esrf.fr'
 
