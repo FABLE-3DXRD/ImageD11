4754f25375f84ca4132ace7c792e102f01248dbb
Jonathan Wright <wright@esrf.fr>
2019-09-12 17:29:31 +0200

    and for windows/gcc recent

--- sandbox/avxsum/imsum.c
+++ sandbox/avxsum/imsum.c
@@ -2,16 +2,55 @@
 #include <immintrin.h>
 #include <emmintrin.h>
 #include <stdint.h> 
+#include <omp.h>
 
-int64_t imsum_avx2_1( uint16_t im[], int n ){
-  __m256i s0,s1;
-  __m128i l, lo, hi, z;
- int i, ni;
+int64_t imsum_avx2_2( uint16_t im[], int64_t n ){
+  __m256i s0,s1,s2,s3;
+  __m128i l0, l1;
+ int i;
  int64_t sum;
- z = _mm_setzero_si128();
  s0 = _mm256_setzero_si256();
  s1 = _mm256_setzero_si256();
- ni = 4;
+ s2 = _mm256_setzero_si256();
+ s3 = _mm256_setzero_si256();
+ for( i = 0; i < n; i = i+16){
+     /* read 4*16=64 bytes, so not aligned 
+      * goes to lower 4*32=128 in t
+      */
+   l0 = _mm_loadu_si128( (__m128i*) &im[i] ) ;
+   s0 = _mm256_add_epi64( s0, _mm256_cvtepu16_epi64 ( l0 ) );
+   s1 = _mm256_add_epi64( s1, _mm256_cvtepu16_epi64 ( _mm_bsrli_si128( l0,  8 )));
+   l1 = _mm_loadu_si128( (__m128i*) &im[i+8] ) ;
+   s2 = _mm256_add_epi64( s2, _mm256_cvtepu16_epi64 ( l1 ) );
+   s3 = _mm256_add_epi64( s3, _mm256_cvtepu16_epi64 ( _mm_bsrli_si128( l1,  8 )));
+ }
+ sum = _mm256_extract_epi64 (s0, 0) + \
+       _mm256_extract_epi64 (s0, 1) + \
+       _mm256_extract_epi64 (s0, 2) + \
+       _mm256_extract_epi64 (s0, 3) ;
+ sum +=_mm256_extract_epi64 (s1, 0) +     \
+       _mm256_extract_epi64 (s1, 1) + \
+       _mm256_extract_epi64 (s1, 2) + \
+       _mm256_extract_epi64 (s1, 3);
+  sum += _mm256_extract_epi64 (s2, 0) + \
+       _mm256_extract_epi64 (s2, 1) + \
+       _mm256_extract_epi64 (s2, 2) + \
+       _mm256_extract_epi64 (s2, 3);
+  sum+=      _mm256_extract_epi64 (s3, 0) + \
+       _mm256_extract_epi64 (s3, 1) + \
+       _mm256_extract_epi64 (s3, 2) + \
+       _mm256_extract_epi64 (s3, 3) ;
+ return sum;
+}
+
+
+int64_t imsum_avx2_1( uint16_t im[], int64_t n ){
+  __m256i s0,s1;
+  __m128i l, lo, hi, z;
+ int i;
+ int64_t sum;
+ s0 = _mm256_setzero_si256();
+ s1 = _mm256_setzero_si256();
  for( i = 0; i < n; i = i+8){
      /* read 4*16=64 bytes, so not aligned 
       * goes to lower 4*32=128 in t
@@ -31,14 +70,12 @@ int64_t imsum_avx2_1( uint16_t im[], int n ){
  return sum;
 }
 
-int64_t imsum_avx2_0( uint16_t im[], int n ){
+int64_t imsum_avx2_0( uint16_t im[], int64_t n ){
   __m256i s;
-  __m128i l, lo, hi, z;
- int i, ni;
+  __m128i l;
+ int i;
  int64_t sum;
- z = _mm_setzero_si128();
  s = _mm256_setzero_si256();
- ni = 4;
  for( i = 0; i < n; i = i+4){
      /* read 4*16=64 bytes, so not aligned 
       * goes to lower 4*32=128 in t
@@ -55,7 +92,7 @@ int64_t imsum_avx2_0( uint16_t im[], int n ){
 
 
 
-int64_t imsum_sse2_0( uint16_t im[], int n ){
+int64_t imsum_sse2_0( uint16_t im[], int64_t n ){
   __m128i s0,s1,s2,s3,t,s32,l;
   int i;
   int64_t sum;
@@ -85,7 +122,7 @@ int64_t imsum_sse2_0( uint16_t im[], int n ){
  return sum;
 }
 
-int64_t imsum_sse2_1( uint16_t im[], int n ){
+int64_t imsum_sse2_1( uint16_t im[], int64_t n ){
   __m128i s1,s2,t,s32,l;
   int i;
   int64_t sum;
@@ -110,11 +147,20 @@ int64_t imsum_sse2_1( uint16_t im[], int n ){
 
 
 
-int64_t imsum_c( uint16_t im[], int n ){
+int64_t imsum_c( uint16_t im[], int64_t n ){
   int i;
   int64_t sum;
-  sum = 0;
   for( i = 0; i < n; i++)
     sum += im[i];
   return sum;
 }
+
+int64_t imsum_c_simd( uint16_t im[], int64_t n ){
+  int i;
+  int64_t sum;
+#pragma omp parallel for simd reduction(+:sum)
+  for( i = 0; i < n; i++)
+    sum += im[i];
+  return sum;
+}
+
