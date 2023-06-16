167c4ba05ef92d1513dfbcc7079c4e072d93ff37
Jonathan Wright <wright@esrf.fr>
2023-06-16 15:53:07 +0200

    assemble many sparse

--- ImageD11/sinograms/assemble_label.py
+++ ImageD11/sinograms/assemble_label.py
@@ -1,114 +1,59 @@
 
-"""WARNING: work in progresss"""
-# FIXME!!!! import ImageD11.sinograms.dataset !!!
-
 from __future__ import print_function
 import h5py, os, time, numpy as np
+
+"""WARNING: work in progresss"""
+
+import ImageD11.sinograms.dataset
+
 
 # given sample + dset -> make a single file with all the sparse pixels in it.
 #  ... this is not really needed, but saves redoing the pixel labelling code
 
 
-
-SCANMOTORS = ("diffrz", "diffrz_cen360", "diffrz_center", "fpico4", "fpico3", "diffty", "diffty_center")
-HEADERMOTORS = ("diffty", "diffrz", "samtx", "samty", "samtz", "diffry", "samrx", "samry")
-
-SCANMOTORS = ("rot", "rot_cen360", "rot_center", "fpico6", "dty", "dty_center")
-HEADERMOTORS = ("dty", "rot", "pz", "px", "py", "shtz", "shty", "shtx")
-DETECTORNAME = 'eiger'
-
-def get_sparse_file_name( eigerfile, rawpath ):
-    """
-    Map the eiger file name onto the sparse peaks filename stored in rawpath
-    """
-    return os.path.join(rawpath, eigerfile.replace("/eiger","_eiger").replace('.h5','_sparse.h5') )
-#    return os.path.join(rawpath, eigerfile.replace(".h5","_sparse.h5"))
+SCANMOTORS = ("diffrz", "diffrz_cen360", "diffrz_center", "fpico4", "fpico3", "diffty", "diffty_center",
+              "rot", "rot_cen360", "rot_center", "fpico6", "dty", "dty_center")
+HEADERMOTORS = ("diffty", "diffrz", "samtx", "samty", "samtz", "diffry", "samrx", "samry",
+                "dty", "rot", "pz", "px", "py", "shtz", "shty", "shtx")
 
 
-def getsparsepath( sample, dsname ):
-    return os.path.join( os.getcwd() + '/sparse', sample, dsname )
+def testready(dsobj):
+    """ assume they are finish if they exist. Might be still writing ??? """
+    done, todo = dsobj.check_sparse()
+    return (done>0) and (todo == 0)
+                                                       
 
-def getoutpath( sample, dsname ):
-    return os.path.join( os.getcwd() + '/sparse', dsname + "_sparse.h5" )
-
-def testready(experiment, sample, dsname):
-    ok = []
-    with h5py.File( os.path.join(experiment, sample, dsname, dsname + '.h5'), 'r' ) as hin:
-        scans = list(hin['/'])
-        for scan in scans:
-            if scan.endswith('.2'):
-                continue
-            if ('measurement' not in hin[scan]) or ('eiger' not in hin[scan]['measurement']):
-                print("Missing measurement/eiger", sample, dsname, scan)
-                return False
-                                   
-            dso = hin[scan]['measurement/eiger']
-            
-            for vs in dso.virtual_sources():
-                ef = get_sparse_file_name( vs.file_name, getsparsepath(sample, dsname)) 
-                done = os.path.exists(ef)
-                if not done:
-                    print("?",sample, dsname, vs.file_name, ef)
-                    return False
-    return True
-
-#assert testready("KPLNO","KPLNO_z50")
-                                                   
-
-def getpixels( dset, samplename, dsetname ):
+def getsparse( dsobj, num, titles = ('row','col','intensity','nnz') ):
     """ 
-    dset = h5py DataSet object for the eiger detector
-    sparsefilename = converter function that takes eiger filename and gives sparse name
-    
+    dsobj = DataSet object
+    returns the pixels from the sparse segmentation
     """
-    pixels = {}
-    titles = 'row','col','intensity','nnz'
-    for t in titles:
-        pixels[t] = []
-    frame0 = 0
-    for vs in dset.virtual_sources():
-        fsparse = ef = get_sparse_file_name( vs.file_name, getsparsepath(samplename, dsetname)) 
-        with h5py.File(  fsparse, "r" ) as hin:
-            fds = vs.dset_name
-            for name in 'row','col','intensity','nnz':
-                pixels[name].append( hin[vs.dset_name][name][:] )# read the data
-#            pixels['frame'].append( hin[vs.dset_name]['frame'][:] + frame0 ) # fir
-            frame0 += vs.src_space.shape[0]
-    for t in titles:
-        pixels[t] = np.concatenate(pixels[t])
+    with h5py.File( os.path.join( dsobj.analysispath, dsobj.sparsefiles[num]) , "r" ) as hin:
+        pixels = { name : hin[dsobj.limapath][name][:] for name in titles }
     return pixels
 
 
-def segment_scans_assemble_raw( experiment, sample, dsetname,
-                   scanmotors=SCANMOTORS,
-                   headermotors=HEADERMOTORS,
-                   detector=DETECTORNAME ):
-    rawpath = getsparsepath( sample, dsetname )
-    fname = os.path.join( experiment, sample, dsetname, dsetname+".h5" )
-    outname = getoutpath( sample, dsetname )
-    if os.path.exists( outname ):
-        print("Already done", outname)
-        return outname
-    print("# output", outname)
-    print("# input", fname, rawpath)
-    
+def harvest_masterfile( dset, outname,
+                        scanmotors=SCANMOTORS,
+                        headermotors=HEADERMOTORS, ):
+    """
+    dset = ImageD11.sinograms.dataset.DataSet object
+    outname = sparse file to write
+    """    
     opts = {
         "chunks": (10000,),
         "maxshape": (None,),
         "compression": "lzf",
         "shuffle": True,
     }
-    ndone = 0
     with h5py.File(outname, "a") as hout:
-        hout.attrs["h5input"] = fname
-        with h5py.File(fname, "r") as hin:
-            scans = list(hin['/'])
-            for scan in scans:
-                if scan.endswith(".2"):  # for fscans
-                    continue
+        hout.attrs["h5input"] = dset.masterfile
+        print("Harvesting",dset.masterfile,end=": ")
+        with h5py.File(dset.masterfile, "r") as hin:
+            for scan in dset.scans:
                 gin = hin[scan]
                 bad = False
-                for check in ('title','measurement','measurement/'+detector):
+                for check in ('title','measurement','measurement/'+dset.detector):
                     if check not in hin[scan]:
                         print(scan,"missing",check,'skipping')
                         bad = True
@@ -116,45 +61,68 @@ def segment_scans_assemble_raw( experiment, sample, dsetname,
                     print("Skipping", scan)
                     continue
                 title = hin[scan]["title"][()]
-                print(scan, end=',')
-                g = hout.create_group(scan)
-                gm = g.create_group("measurement")
+                g = hout.require_group(scan)
+                gm = g.require_group("measurement")
                 for m in scanmotors:  # vary : many
                     if m in gin["measurement"]:
-                        gm.create_dataset(m, data=gin["measurement"][m][:])
-                gip = g.create_group("instrument/positioners")
+                        data = data=gin["measurement"][m][:]
+                        ds = gm.require_dataset(m, shape=data.shape, dtype = data.dtype )
+                        ds[()] = data
+                gip = g.require_group("instrument/positioners")
                 for m in headermotors:  # fixed : scalar
                     if "instrument/positioners/%s"%(m) in gin:
-                        gip.create_dataset(m, data=gin["instrument/positioners"][m][()])
+                        data=gin["instrument/positioners"][m][()]
+                        ds  = gip.require_dataset(m, shape = data.shape, dtype = data.dtype )
+                        ds[()] = data
                 try:
-                    frms = gin["measurement"][detector]
-                    pixels = getpixels( frms, sample, dsetname )
-                except:
+                    frms = gin["measurement"][dset.detector]
+                except Exception as e:
+                    print(e)
                     print(list(gin))
                     print(list(gin["measurement"]))
                     print(detector)
-                    raise
-                # FIXME : can't we do this using external links?
-                opts['chunks'] = ( len(pixels['nnz']), )
-                g.create_dataset("row", data=pixels['row'], dtype=np.uint16, **opts)
-                g.create_dataset("col", data=pixels['col'], dtype=np.uint16, **opts)
-                # can go over 65535 frames in a scan
-#                g.create_dataset("frame", data=pixels['frame'], dtype=np.uint32, **opts)
-                g.create_dataset("intensity", data=pixels['intensity'], dtype=frms.dtype, **opts)
-                g.create_dataset("nnz", data=pixels['nnz'], dtype=np.uint32)
-                g.attrs["itype"] = np.dtype(np.uint16).name
+                    raise        
+                g.attrs["itype"] = frms.dtype.name
                 g.attrs["nframes"] = frms.shape[0]
                 g.attrs["shape0"] = frms.shape[1]
                 g.attrs["shape1"] = frms.shape[2]
-            print("\n# Done", scan)
+                print(scan, end=', ')
+            print()
+                        
+        # Finished with master file. Now harvest the segmented files.
+        idx = 0
+        titles = ('row','col','intensity','nnz')
+        print('Loading pixels:',end=' ')
+        for scan in dset.scans:
+            g = hout.require_group( scan )
+            for name in 'row', 'col':
+                if name not in g:
+                    g.create_dataset(name, shape = (0,), dtype=np.uint16, **opts)
+            if "intensity" not in g:
+                g.create_dataset("intensity", shape = (0,), dtype=g.attrs['itype'], **opts)
+            nfrm = g.attrs['nframes']
+            g.require_dataset("nnz", shape = (nfrm,), dtype=np.uint32)
+            nstart = nread = npx = pstart = 0
+            while nread < nfrm:
+                pixels = getsparse( dset, idx, titles )
+                idx += 1 # loop over sparse files in this scan
+                nread = nstart + len(pixels['nnz']) # number of frames in this limafile
+                g['nnz'][nstart : nread] = pixels['nnz']
+                nstart = nread
+                pread = pstart + len(pixels['row']) # number of pixels in this limafile
+                for name in 'row', 'col', 'intensity':
+                    g[name].resize( (pread, ) )
+                    g[name][pstart:pread] = pixels[name]
+                pstart = pread
+            print(scan,end=', ')
+        print()
     return outname
 
-
-def main( experiment, samples_dsets ):
-        # experiment= '/data/visitor/hc5185/id11/20230505/RAW_DATA'
-    for sample , dset in samples_dsets:
-        outname = segment_scans_assemble_raw(experiment, sample, "{sample}_{dset}".format(**locals()) )
-        
-        
-        
     
+def main( dsname, outname ):
+    dset = ImageD11.sinograms.dataset.load( dsname )
+    harvest_masterfile( dset, outname )
+    
+if __name__=="__main__":
+    import sys
+    main( sys.argv[1], sys.argv[2] )
