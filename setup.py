94b83279f2ec40fa30590dd76649a8c6e49862ec
Jonathan Wright <wright@esrf.fr>
2023-09-12 14:13:23 +0200

    fixing dependencies?

--- setup.py
+++ setup.py
@@ -156,12 +156,6 @@ useful = [   # stuff you probably want, and should be able to get easily
     'h5py',
     'pyyaml',
     "pytest",       # for the CI
-]
-
-
-more = [
-    # Used in sandbox / test / not completely essential, but should work for CI
-
     'numba==0.46.0 ; python_version < "3" ',       # for some test cases
     'numba ; python_version > "3" ',               # for some test cases
     "bslz4_to_sparse",
@@ -169,6 +163,11 @@ more = [
     "scikit-image",
     "hdf5plugin",
     "tqdm",
+]
+
+
+more = [
+    # Used in sandbox / test / not completely essential, but should work for CI
     "pillow",      # in sandbox
     "lmfit",       # in sandbox
     "PyMca5",      # in sandbox
