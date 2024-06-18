8b732a229d0f629404d3a70854f909b52cbc3b05
Jonathan Wright <wright@esrf.fr>
2024-06-18 14:40:20 +0200

    numpy 2, the adventure begins

--- setup.py
+++ setup.py
@@ -142,7 +142,8 @@ scripts = ["ImageD11/rsv_mapper.py",
 
 minimal = [  # can't compile without this
     "six",
-    "numpy",
+    'numpy ; python_version < "3"',
+    'numpy>=1.23.5 ; python_version >= "3"',
     "setuptools",
     ]
 
