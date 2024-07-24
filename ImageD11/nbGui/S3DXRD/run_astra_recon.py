c479485ee6a171310dc84854437376cce3b8abd1
James Ball <jadball@gmail.com>
2024-07-24 10:16:46 +0200

    Run Astra on cluster rather than locally, replace cluster CPU MLEM method

---
+++ ImageD11/nbGui/S3DXRD/run_astra_recon.py
@@ -0,0 +1,27 @@
+def main(h5name, dsfile):
+    # load the dataset
+    ds = ImageD11.sinograms.dataset.load(dsfile)
+
+    grainsinos = read_h5(h5name, ds)
+
+    for gs in grainsinos:
+        gs.recon(method="astra", astra_method="EM_CUDA")
+
+    write_h5(h5name, grainsinos, write_grains_too=True)
+
+
+if __name__ == "__main__":
+    # horrible workaround to include id11 code path
+    import sys
+    
+    id11_code_path = sys.argv[1]
+    
+    sys.path.insert(0, id11_code_path)
+
+    from ImageD11.sinograms.sinogram import read_h5, write_h5
+    import ImageD11.sinograms.dataset
+    
+    h5name = sys.argv[2]
+    dsfile = sys.argv[3]
+    
+    main(h5name, dsfile)
