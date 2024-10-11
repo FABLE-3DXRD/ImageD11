186d6cd068c5de56df80f4383dd4cfe1bb20d864
James Ball <jadball@gmail.com>
2024-10-11 10:27:26 +0200

    Run PBP refinement on cluster

--- ImageD11/nbGui/S3DXRD/run_pbp_refine.py
+++ ImageD11/nbGui/S3DXRD/run_pbp_refine.py
@@ -1,55 +1,17 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
+import sys
+from ImageD11.sinograms.point_by_point import PBPRefine
+
 if __name__ == "__main__":
-
-    import sys
-    import ImageD11.sinograms.point_by_point
-    import ImageD11.sinograms.dataset
-    from ImageD11 import cImageD11
-    import ast
-
-    dsfile = sys.argv[1]
-    hkltol = float(sys.argv[2])
-    fpks = float(sys.argv[3])
-    dstol = float(sys.argv[4])
-    etacut = float(sys.argv[5])
-    ifrac = float(sys.argv[6])
-    costol = float(sys.argv[7])
-    y0 = float(sys.argv[8])
-    symmetry = sys.argv[9]
-    foridx = [int(x) for x in ast.literal_eval(sys.argv[10])]
-    forgen = [int(x) for x in ast.literal_eval(sys.argv[11])]
-    uniqcut = float(sys.argv[12])
-    phase_name = sys.argv[13]
-    minpkint = int(sys.argv[14])
-
-    if phase_name == str(None):
-        phase_name = None
-
-    print('Loading dset')
-    ds = ImageD11.sinograms.dataset.load(dsfile)
-
-    print('Loading peaks')
-    ImageD11.cImageD11.cimaged11_omp_set_num_threads(ImageD11.cImageD11.cores_available())
-    cf_2d = ds.get_cf_2d()
-    ImageD11.cImageD11.cimaged11_omp_set_num_threads(1)
-    cf_2d.filter(cf_2d.Number_of_pixels > minpkint)
-
-    print('Making pbp object')
-    pbp_object = ImageD11.sinograms.point_by_point.PBP(ds.parfile,
-                                                       ds,
-                                                       hkl_tol=hkltol,
-                                                       fpks=fpks,
-                                                       ds_tol=dstol,
-                                                       etacut=etacut,
-                                                       ifrac=ifrac,
-                                                       cosine_tol=costol,
-                                                       y0=y0,
-                                                       symmetry=symmetry,
-                                                       foridx=foridx,
-                                                       forgen=forgen,
-                                                       uniqcut=uniqcut,
-                                                       phase_name=phase_name)
-
-    pbp_object.setpeaks(cf_2d)
-
-    print('Go for pbp')
-    pbp_object.point_by_point(ds.pbpfile, loglevel=3)
+    pbprefinefile = sys.argv[1]
+    output_filename = sys.argv[2]
+    print('Loading refinement object from disk')
+    refine = PBPRefine.from_h5(pbprefinefile)
+    print('Running refinement')
+    refine.run_refine(output_filename=output_filename, use_cluster=False)
+    print('Refinement complete!')
