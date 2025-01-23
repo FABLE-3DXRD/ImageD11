07885c572981950bcf9754ebd9716e87a20c1267
James Ball <jadball@gmail.com>
2025-01-23 22:05:37 +0100

    Full tomographic route end-to-end now validated on real data

--- test/papermill_test_notebooks.py
+++ test/papermill_test_notebooks.py
@@ -32,13 +32,14 @@ scan_nb_prefix = os.path.join(nb_base_prefix, 'S3DXRD')
 
             
 def noteboook_exec_pmill(nb_input_path, nb_output_path, params_dict):
+    print('Executing notebook', nb_input_path)
     papermill.execute_notebook(
        nb_input_path,
        nb_output_path,
        parameters=params_dict
     )
 
-def notebook_route(base_dir, notebook_paths, notebook_param_dicts):
+def notebook_route(base_dir, notebook_paths, notebook_param_dicts, notebook_out_dir=None):
     """
     Execute multiple notebooks in the order they are given.
     base_dir: The path to the output folder for the test. Must not already exist.
@@ -48,7 +49,8 @@ def notebook_route(base_dir, notebook_paths, notebook_param_dicts):
     if os.path.exists(base_dir):
         raise ValueError('output test directory already exists:', base_dir)
     os.mkdir(base_dir)
-    notebook_out_dir = os.path.join(base_dir, 'nb_out')
+    if notebook_out_dir is None:
+        notebook_out_dir = os.path.join(base_dir, 'nb_out')
     os.mkdir(notebook_out_dir)
     for notebook_in_path, notebook_param_dict in zip(notebook_paths, notebook_param_dicts):
         notebook_out_path = os.path.join(notebook_out_dir, os.path.split(notebook_in_path)[1].replace('.ipynb', '_out.ipynb'))
@@ -110,7 +112,7 @@ def test_tomographic_route():
          'ifrac': 7e-3,
          'use_cluster': False
         },
-        {'PYTHONPATH': sys.path[0],  # '4_visualise.ipynb
+        {'PYTHONPATH': sys.path[0],  # 4_visualise.ipynb
          'dset_file': dset_file,
          'phase_str': 'Si',
          'min_unique': 400,
@@ -120,7 +122,7 @@ def test_tomographic_route():
     notebook_route(tomo_dir, scan_nb_paths, scan_nb_params)
     
 
-    
+
 # test the full point-by-point route from start to finish
 def test_pbp_route():
     tomo_dir = 'pbp_route'                    
@@ -177,7 +179,163 @@ def test_pbp_route():
     ]
     scan_nb_paths = [os.path.join(scan_nb_prefix, name) for name in scan_nb_names]
     notebook_route(tomo_dir, scan_nb_paths, scan_nb_params)
-    
+
+
+def test_FeAu_JADB_tomo():
+    tomo_dir = '/data/id11/inhouse2/test_data_3DXRD/S3DXRD/FeAu/PROCESSED_DATA/20250123_JADB/tomo_route'                    
+    scan_nb_names = [
+        '0_segment_and_label.ipynb',
+        'tomo_1_index.ipynb',
+        'tomo_1_index_minor_phase.ipynb',
+        'tomo_2_map.ipynb',
+        'tomo_2_map_minor_phase.ipynb',
+        'tomo_3_refinement.ipynb',
+        'tomo_3_refinement_minor_phase.ipynb',
+        '4_visualise.ipynb',  # for major phase
+        '4_visualise.ipynb',  # for minor phase
+        '5_combine_phases.ipynb',
+        '6_stack_layers.ipynb'
+        
+    ]
+    sample = 'FeAu_0p5_tR_nscope'
+    dataset = 'top_200um'  # first of two layers
+    dset_file = os.path.join(tomo_dir, sample, f'{sample}_{dataset}', f'{sample}_{dataset}_dataset.h5')
+    scan_nb_params = [
+        {'PYTHONPATH': sys.path[0],  # 0_segment_and_label.ipynb
+         'maskfile': '/data/id11/inhouse2/test_data_3DXRD/S3DXRD/FeAu/pars/mask_with_gaps_E-08-0173.edf',
+         'e2dxfile': '/data/id11/inhouse2/test_data_3DXRD/S3DXRD/FeAu/pars/e2dx_E-08-0173_20231127.edf',
+         'e2dyfile': '/data/id11/inhouse2/test_data_3DXRD/S3DXRD/FeAu/pars/e2dy_E-08-0173_20231127.edf',
+         'detector': 'eiger',
+         'omegamotor': 'rot_center',
+         'dtymotor': 'dty',
+         'options': { 'cut' : 1, 'pixels_in_spot' : 3, 'howmany' : 100000 },
+         'dataroot': '/data/id11/inhouse2/test_data_3DXRD/S3DXRD/FeAu/RAW_DATA/',
+         'analysisroot': tomo_dir,
+         'sample': 'FeAu_0p5_tR_nscope',
+         'dataset': 'top_200um'
+        },
+        {'PYTHONPATH': sys.path[0],  # tomo_1_index.ipynb
+         'dset_file': dset_file,
+         'par_file': '/data/id11/inhouse2/test_data_3DXRD/S3DXRD/FeAu/pars/pars.json',
+         'phase_str': 'Fe',
+         'cf_strong_frac': 0.9939,
+         'cf_strong_dsmax': 1.594,
+         'cf_strong_dstol': 0.005,
+         'indexer_ds_tol': 0.01,
+         'rings_for_gen': [0, 1, 3],
+         'rings_for_scoring': [0, 1, 2, 3, 4],
+         'hkl_tols_seq': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.075],
+         'fracs': [0.9, 0.7],
+         'max_grains': 1000,
+         'peak_assign_tol': 0.05,
+        },
+        {'PYTHONPATH': sys.path[0],  # tomo_1_index_minor_phase.ipynb
+         'dset_file': dset_file,
+         'major_phase_str': 'Fe',
+         'minor_phase_str': 'Au',
+         'major_phase_cf_frac': 0.99418,
+         'major_phase_cf_dsmax': 1.594,
+         'major_phase_cf_dstol': 0.0035,
+         'minor_phase_cf_frac': 0.9975,
+         'minor_phase_cf_dsmax': 1.594,
+         'minor_phase_cf_dstol': 0.0045,
+         'indexer_ds_tol': 0.0045,
+         'rings_for_gen': [0, 4, 5],
+         'rings_for_scoring': [0, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13],
+         'hkl_tols_seq': [0.01, 0.02, 0.03, 0.04, 0.05],
+         'fracs': [0.9],
+         'max_grains': 1000,
+         'peak_assign_tol': 0.05,
+        },
+        {'PYTHONPATH': sys.path[0],  # tomo_2_map.ipynb
+         'dset_file': dset_file,
+         'phase_str': 'Fe',
+         'cf_strong_frac': 0.9939,
+         'cf_strong_dstol': 0.005,
+         'is_half_scan': False,
+         'halfmask_radius': 25,
+         'peak_assign_tol': 0.25,
+         'manual_threshold': None,
+         'hkltol': 0.25,
+         'correct_sinos_with_ring_current': True,
+         'first_tmap_cutoff_level': 0.4,
+         'niter': 500,
+         'second_tmap_cutoff_level': 0.05
+        },
+        {'PYTHONPATH': sys.path[0],  # tomo_2_map_minor_phase.ipynb
+         'dset_file': dset_file,
+         'major_phase_str': 'Fe',
+         'minor_phase_str': 'Au',
+         'major_phase_cf_frac': 0.994,
+         'major_phase_cf_dstol': 0.005,
+         'minor_phase_cf_frac': 0.9975,
+         'minor_phase_cf_dstol': 0.005,
+         'is_half_scan': False,
+         'halfmask_radius': 25,
+         'peak_assign_tol': 0.25,
+         'hkltol': 0.25,
+         'correct_sinos_with_ring_current': True,
+         'first_tmap_cutoff_level': 0.4,
+         'niter': 500,
+         'second_tmap_cutoff_level': 0.5,
+         'grain_too_many_px': 10
+        },
+        {'PYTHONPATH': sys.path[0],  # tomo_3_refinement.ipynb
+         'dset_file': dset_file,
+         'phase_str': 'Fe',
+         'default_npks': 20,
+         'default_nuniq': 20,
+         'hkl_tol_origins': 0.05,
+         'hkl_tol_refine': 0.1,
+         'hkl_tol_refine_merged': 0.05,
+         'ds_tol': 0.004,
+         'ifrac': 7e-3,
+         'use_cluster': False,
+        },
+        {'PYTHONPATH': sys.path[0],  # tomo_3_refinement_minor_phase.ipynb
+         'dset_file': dset_file,
+         'major_phase_str': 'Fe',
+         'minor_phase_str': 'Au',
+         'rings_to_refine': [0, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13],
+         'default_npks': 20,
+         'default_nuniq': 20,
+         'hkl_tol_origins': 0.05,
+         'hkl_tol_refine': 0.1,
+         'hkl_tol_refine_merged': 0.05,
+         'ds_tol': 0.006,
+         'ifrac': 1e-3,
+         'use_cluster': False,
+        },
+        {'PYTHONPATH': sys.path[0],  # 4_visualise.ipynb - major phase
+         'dset_file': dset_file,
+         'phase_str': 'Fe',
+         'is_minor_phase': False,
+         'min_unique': 250,
+        },
+        {'PYTHONPATH': sys.path[0],  # 4_visualise.ipynb - minor phase
+         'dset_file': dset_file,
+         'phase_str': 'Au',
+         'is_minor_phase': True,
+         'min_unique': 0,
+        },
+         {'PYTHONPATH': sys.path[0],  # 5_combine_phases.ipynb
+         'dset_file': dset_file,
+         'phase_strs': ['Fe', 'Au'],
+         'combine_refined': True,
+        },
+         {'PYTHONPATH': sys.path[0],  # 6_stack_layers.ipynb
+         'dset_file': dset_file,
+         'stack_combined': True,
+         'stack_refined': True,
+         'zstep': 50.0,
+        },
+    ]
+    if len(scan_nb_names) != len(scan_nb_params):
+        raise ValueError('Mismatch between number of notebooks and param dicts!')
+    scan_nb_paths = [os.path.join(scan_nb_prefix, name) for name in scan_nb_names]
+    notebook_route(tomo_dir, scan_nb_paths, scan_nb_params)
+
+
 if __name__=='__main__':
-    print(ImageD11.__path__)
     print(papermill.__path__)
+    test_FeAu_JADB_tomo()
