3cf73079d20449267def3b22b4d3810b81155cc2
Jonathan Wright <wright@esrf.fr>
2025-01-24 18:15:13 +0100

    make sandbox importable #255

---
+++ ImageD11/sandbox/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+ImageD11.sandbox is for development/testing and not general usage
+
+If there is something in here that you need then please clean it up
+and do 'git mv my_useful_thing.py somewhere_else'
+"""
+
